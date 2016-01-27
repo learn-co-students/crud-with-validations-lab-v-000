a1fd0f4727f310845ec8eda98f24ffa33e28a2d0
Justin Thomas <jthomas0209@gmail.com>
2016-01-27 14:47:54 -0500

    Changed delete link to utilize link_to

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  resources :songs
+  resources :songs, only:[:index,:create, :new, :show, :edit, :update]
+  get "/songs/:id/delete", to: "songs#destroy", as: "delete_song"
 end
