cd1e2a645b48951dd55d68f51b6970be1e8c924d
Shana L Moore <shana.lavina.moore@gmail.com>
2016-02-03 12:06:03 -0800

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,13 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true 
+  validates :released, inclusion: {in: [true, false]}
+  validates :artist_name, presence: true
+  validates :title, uniqueness: {scope: :release_year, message: "Cannot be repeated by the same artist in the same year!"}
+  validates :release_year, numericality: { less_than: Time.now.year }, :allow_nil => true
+  validates :release_year, presence: true, if: :released?
+
+  def released?
+    released == true
+  end
+
+end
