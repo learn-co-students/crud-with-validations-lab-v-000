afe04c5687b2aa527c4376708ee6c0e9cfa9fb6c
eric-an <erican@me.com>
2016-02-18 16:12:52 -0800

    create CRUD app with validations

---
+++ app/models/song.rb
@@ -0,0 +1,15 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: { scope: [:release_year, :artist_name], 
+    message: "title is only available once a year to each artist" }
+  validates :released, inclusion: { in: [true, false] }
+  validates :artist_name, presence: true
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: { less_than_or_equal_to: Time.now.year }
+  end
+
+  def released?
+    released
+  end
+
+end
