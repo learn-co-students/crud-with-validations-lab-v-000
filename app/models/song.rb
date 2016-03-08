80e2f199852f2646834db454b3afa937d8295610
pacosta613 <pedro.acosta613@gmail.com>
2016-03-08 15:23:33 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,8 @@
 class Song < ActiveRecord::Base
-  validates :title, :artist_name, presence: true, uniqueness: true
+  validates :title, presence: true, uniqueness: true
   validates :release_year, numericality: { less_than: Time.now.year }, :allow_nil => true
   validates :release_year, presence: true, if: :released?
+  validates :artist_name, presence: true
 
   def released?
     released == true
