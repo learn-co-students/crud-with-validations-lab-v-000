0b4cd2a03112a26385c4128cda77e48798d68db2
pickledyamsman <pickledyamsman@gmail.com>
2016-03-15 16:37:53 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,7 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, uniqueness: {scope: :release_year}
+  validates :released, inclusion: {in: [true, false]}
+  validates :release_year, if: :released, presence: true, numericality: {:less_than_or_equal_to => Date.today.year}
+
+end
