0136d1412fbba761b3d6bd36af628f8f11d98d8f
e-baker <eric@ericbaker.me>
2016-04-20 14:08:59 -0400

    all tests but one pass -> index page spec wants table?

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,19 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  #validates :title, uniqueness:   ***TODO: FIGURE THIS OUT ***
+  validates :title, uniqueness: {
+    scope: [:release_year, :artist_name],
+    message: "An artist can't repeat the same song in the same year."
+  }
   validates :released, inclusion: { in: [true, false] }
   validates :artist_name, presence: true
-  #TODO: figure out how to validate the released year if released
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {
+      less_than_or_equal_to: Time.now.year
+    }
+  end
 
-  
+  def released?
+    released
+  end
 end
