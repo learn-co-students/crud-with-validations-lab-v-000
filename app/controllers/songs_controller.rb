0a86eca13d56d6fc067e59872bded694fde5a230
e-baker <eric@ericbaker.me>
2016-04-20 13:19:36 -0400

    finish views and controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,4 +11,37 @@ class SongsController < ApplicationController
   def new
     @song = Song.new
   end
+
+  def create
+    @song = Song.new(params.require(:song).permit(:title, :release_year, :released, :genre, :artist_name))
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(params.require(:song).permit(:title, :release_year, :released, :genre, :artist_name))
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @song = Song.find(params[:id])
+    if @song.destroy
+      redirect_to songs_url
+    else
+      redirect_to song_path(@song)
+    end
+  end
+    else
+
 end
