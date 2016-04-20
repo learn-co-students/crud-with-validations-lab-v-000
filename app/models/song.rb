f4712fbc831dd1fdb0fb7a75f0f759688d7923bb
e-baker <eric@ericbaker.me>
2016-04-20 12:28:18 -0400

    Start setup of model validations.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,9 @@
 class Song < ActiveRecord::Base
+  validates :title, presence: true
+  #validates :title, uniqueness:   ***TODO: FIGURE THIS OUT ***
+  validates :released, inclusion: { in: [true, false] }
+  validates :artist_name, presence: true
+  #TODO: figure out how to validate the released year if released
+
+  
 end
