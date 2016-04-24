0612c6c539289332d045b6243a6d9b7eb2ce5589
KhalilC <klcrumpler@gmail.com>
2016-04-24 17:16:42 -0400

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+valid_attributes = {
+      title: "Talisman",
+      artist_name: "Air",
+      release_year: 2007,
+      released: true,
+      genre: "Post-Rock"
+    }
+
+
+Song.create(valid_attributes)
