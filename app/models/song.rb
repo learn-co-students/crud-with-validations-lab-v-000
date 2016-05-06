5d0909137c6c196d6d65239e09e4a4b9271bae5c
hudbuz <hzb2@cornell.edu>
2016-05-06 13:15:32 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,26 @@
 class Song < ActiveRecord::Base
   validates :title, :artist_name, :genre, presence: true
-  validates :release_year, presence: true, if: :released
-  validates :release_year, numericality: {less_than_or_equal_to: 2016}
+  validates :title, uniqueness: {scope: [:artist_name, :release_year]}
+
+
+  with_options if: :released? do |admin|
+    admin.validates :release_year, numericality: { less_than_or_equal_to: 2016 }
+    admin.validates :release_year, presence: true
+  end
+
+  with_options if: !:released? do |admin|
+    
+    admin.validates :release_year, presence: false
+  end
+  
+
+
+
+
+
+  def released?
+    released
+  end
 
 
 end
