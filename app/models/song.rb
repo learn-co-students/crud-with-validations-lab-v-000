e84ed69cd55329cc1b40ffec6fd3b95703bc605d
gnfisher <me@gnfisher.com>
2016-05-17 15:36:17 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,22 +1,20 @@
-
 class Song < ActiveRecord::Base
   validates_presence_of :title
-  validates_presence_of :release_year,
-                        if: Proc.new { |song| song.released } 
-  validate :release_year_cannot_be_in_future, :cannot_release_twice_in_same_year
-
-  private
-
-  def release_year_cannot_be_in_future
-    if release_year.present? && Time.now.year < release_year
-      errors[:release_year] = "cannot be in the future"
-    end
-  end
-
-  def cannot_release_twice_in_same_year
-    if Song.find_by(self.attributes.except("id", "created_at", "updated_at"))
-      errors[:release_year] = "cannot release the same song twice in the same year"
-    end
+  validates :title, uniqueness: {
+    scope: [:release_year, :artist_name],
+    message: "cannot be repeated by the same artist in the same year"
+  }
+  validates :released, inclusion: { in: [true, false] }
+  validates :artist_name, presence: true
+  
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {
+      less_than_or_equal_to: Date.today.year
+    }
   end
   
+  def released?
+    released
+  end
 end
