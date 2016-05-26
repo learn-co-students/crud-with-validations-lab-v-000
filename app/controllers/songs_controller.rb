e407b50276adc9c2e3e782d0bc656818821ef27a
Drew Pham <drewvpham@gmail.com>
2016-05-26 15:40:32 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,14 +14,23 @@ class SongsController < ApplicationController
     @song=Song.new
 end
 
-def update
-    @song = Song.find(params[:id])
-  if @song.update(song_params)
-    redirect_to song_path(@song)
-  else
-    render :edit
-  end
-end
+def create
+   @song = Song.new(song_params)
+   if @song.save
+     redirect_to @song
+   else
+     render :new
+   end
+ end
+
+ def update
+   @song = Song.find(params[:id])
+   if @song.update(song_params)
+     redirect_to @song
+   else
+     render :edit
+   end
+ end
 
 def destroy
    @song = Song.find(params[:id])
