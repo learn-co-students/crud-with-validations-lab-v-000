c39cf5ee0c3cc2715365d255bb587b24b74df039
kriskanya <kriskanya@gmail.com>
2016-06-16 13:04:25 -0500

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,59 @@
+class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    find_song
+  end
+
+  def edit
+    find_song
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+
+    if @song.valid?
+      @song.save
+
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def update
+    find_song
+
+    if @song.update(song_params)
+
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Song.find(params[:id]).destroy
+    redirect_to songs_path
+  end
+
+  private
+
+  def find_song
+    @song = Song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:title, :artist_name, :release_year, :released, :genre)
+  end
+
+end
+
+
