6a67e69423b8fb365e20609c1d02617e131022b8
tttra <tothutra@gmail.com>
2016-06-27 00:20:07 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,14 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates_uniqueness_of :title, scope: [:artist_name, :release_year]
+  validates :artist_name, presence: true
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }
+  end
+
+  def released?
+    released
+  end
+
+end
