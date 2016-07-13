57134af3e16cba7424cbafefcdb42ee6ff46f2a6
mcclains2003 <mcclains2003@gmail.com>
2016-07-13 12:42:32 -0500

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,13 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, uniqueness: {
+    scope: [:release_year, :artist_name],
+    message: "Cannot release duplicate song by same artist in same year"
+  }
+  validates :released, inclusion: { in: [true, false] }
+  with_options if: :released do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: { less_than_or_equal_to: Time.now.year }
+  end
+
+end
