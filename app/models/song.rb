9122813bf9f06bcf5b5727883a3a79723e76c7b1
Christian Whitesides <whitesidescr@gmail.com>
2016-07-18 15:42:16 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,6 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true
-  validates :release_year, presence: true, if: :released
-
-  
-
-  # def release_year_not_in_the_future?
-  #   release_year <= Time.now.year
-  # end
+  validates :title, presence: true, uniqueness: { scope: :release_year }
+  validates :release_year, presence: true, numericality: { less_than_or_equal_to: Date.today.year }, if: :released 
+  validates_inclusion_of :released, in: [true, false]
 end
 
