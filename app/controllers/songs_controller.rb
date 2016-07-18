d3eb4b03121fada626f16f389b58813645e935aa
jbok4 <jbok4@aol.com>
2016-07-18 17:09:00 -0400

    method Song complete

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,29 @@
 class SongsController < ApplicationController
+
+  before_action :set_song!
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @song.update(song_params)
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def song_params
+    params.permit(:title, :released, :release_year, :artist_name, :genre)
+  end
+
+  def set_song!
+    @song = Song.find(params[:id])
+  end
+
 end
