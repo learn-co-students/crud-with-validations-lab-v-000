2b24c7be34106b4cae27e21bddc9c723551dfa74
Avinash Prasad <kirangp@gmail.com>
2016-07-26 23:58:32 +1000

    CRUD validations working in the lab

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,13 +7,43 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
   end
 
+  def new
+    @song = Song.new
+  end
+
   def edit
     @song = Song.find(params[:id])
   end
 
   def create
-    @song = Song.create(params)
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to @song
+    else
+      render :new
+    end
   end
 
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to @song
+    else
+      render :edit
+    end
+  end
 
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+    redirect_to songs_url
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(
+      :title, :release_year, :released, :genre, :artist_name
+    )
+  end
 end
