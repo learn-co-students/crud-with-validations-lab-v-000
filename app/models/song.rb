defba49862ab1666f4e8ec17099f5461a1ca3a11
cmiller36 <m.calliemiller@gmail.com>
2016-08-01 12:21:18 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,8 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, uniqueness: {scope: [:artist_name, :release_year]}
+  validates :released, inclusion: [true,false]
+  validates :release_year, presence: true, if: :released
+  validates :release_year, numericality: {less_than_or_equal_to: Date.current.year}, if: :released
+  validates :artist_name, presence: true
+end
