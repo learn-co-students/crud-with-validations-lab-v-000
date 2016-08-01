30e966e7dd615e8cadfa44b274c3cdee1f7997de
Joe <jsbenning4@gmail.com>
2016-07-31 21:56:34 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,7 @@
-Rails.application.routes.draw do
+Rails.application.routes.draw do 
+  #resource :song
+  resources :songs, only: [:index, :show, :new, :create, :edit]
+  get 'songs', to: 'songs#index'
+  patch 'songs/:id', to: 'songs#update'
+  delete 'songs/:id', to: 'songs#destroy'
 end
