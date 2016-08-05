3e4c858b439f687ea21466ece40f28ba2830f9cc
arturusfury <bri.reynolds@gmail.com>
2016-08-05 15:01:07 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,10 +5,10 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new(song_params(:title, :released, :release_year, :artist_name, :genre))
+    @song = Song.new(song_params)
 
     if @song.save
-      redirect_to song_path(@song)
+      redirect_to @song
     else
       render :new
     end
@@ -30,8 +30,8 @@ class SongsController < ApplicationController
 
   def update
     @song = Song.find(params[:id])
-    if @song.update(song_params(:title, :released, :release_year, :artist_name, :genre))
-      redirect_to song_path(@song)
+    if @song.update(song_params)
+      redirect_to @song
     else
       render :edit
     end
@@ -45,7 +45,9 @@ class SongsController < ApplicationController
 
   private
 
-  def song_params(*args)
-    params.require(:song).permit(*args)
+  def song_params
+    params.require(:song).permit(
+      :title, :released, :release_year, :artist_name, :genre
+    )
   end
 end
