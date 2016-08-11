d313d504b928857a45570af011cc3e84aa691aa1
jwolfe890 <jwolfe890@gmail.com>
2016-08-11 15:37:54 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,7 +4,7 @@ class Song < ActiveRecord::Base
   validates :title, uniqueness: { scope: [:release_year, :artist_name] }
   validates :released, inclusion: { in: [ true, false ] }
   validates :artist_name, presence: true 
-  validates :release_year, numericality: { less_than_or_equal_to: Time.new.year }
+  validates :release_year, numericality: { less_than_or_equal_to: Time.new.year }, if: :released?
   validates :release_year, presence: true, if: :released?
 
   def released?
