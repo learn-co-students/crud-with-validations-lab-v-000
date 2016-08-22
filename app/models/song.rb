d99a255166e05cabb52966cad5bf55eb82d9e7d5
peterberwind <peterberwind@gmail.com>
2016-08-22 17:13:57 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,15 @@
+class Song < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :title, uniqueness: {
+    scope: [:artist_name, :release_year]
+  }
+  validates :released, inclusion: { in: [ true, false ] }
+  validates :artist_name, presence: true
+
+  with_options if: :released? do
+    validates :release_year, presence: true
+    validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }
+  end
+
+end
