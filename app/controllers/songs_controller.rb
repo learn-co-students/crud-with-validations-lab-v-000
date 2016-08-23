6da074ed69cb04913f19ecab0182577ba528c666
MIKE-RANDELL <michael.james.randell@gmail.com>
2016-08-23 08:02:41 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,18 +1,20 @@
 class SongsController < ApplicationController
+  before_action :find_song, only: [:show, :edit, :update]
+
   def index
     @songs = Song.all
   end
 
   def show
-    @song = Song.find(params[:id])
+    #@song = Song.find(params[:id])
   end
 
   def edit
-    @song = Song.find(params[:id])
+    #@song = Song.find(params[:id])
   end
 
   def update
-    @song = Song.find(params[:id])
+    #@song = Song.find(params[:id])
 
     if @song.update(params_check(:title, :artist_name, :release_year, :released, :genre))
       redirect_to song_path(@song)
@@ -36,12 +38,18 @@ class SongsController < ApplicationController
   end
 
   def destroy
+    #binding.pry
     @song = Song.find(params[:id]).destroy
     redirect_to songs_path
   end
 
   private
+
   def params_check(*args)
     params.require(:song).permit(*args)
+  end
+
+  def find_song
+    @song = Song.find(params[:id])
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,11 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :title, uniqueness: {scope: :release_year}
-  validate :released?
-  #validate :release_year_confirmation
+  #validate :released?
   validates :artist_name, presence: true
+  validates :release_year, presence: true, numericality: {less_than_or_equal_to: Time.now.year}, if: :released
 
+=begin
   def released?
     if released && release_year == nil
       errors.add(:release_year, "please give a release date if set as released")
@@ -14,6 +15,6 @@ class Song < ActiveRecord::Base
       errors.add(:release_year, "release date must be before current date")
     end
   end
-
+=end
 
 end
