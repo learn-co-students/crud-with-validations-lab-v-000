07bb5bbba0fe6e5b5d96977bc73098f543ce54ac
Jesse Novotny <jessenovotny@gmail.com>
2016-08-22 17:24:25 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,8 +5,6 @@ class Song < ActiveRecord::Base
   # validates :release_year, inclusion: { in: 1900..Date.today.year }, unless: :released?
   validates :artist_name, presence: true
   with_options if: :released? do |song|
-    
-    binding.pry
     song.validates :release_year, presence: true
     song.validates :release_year, inclusion: { in: 1900..Date.today.year }
   end
