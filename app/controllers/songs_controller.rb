486d18994c67c204d0471cb6e06d34f619b7e4fd
miclp <michaelpeimani@gmail.com>
2016-08-22 17:15:57 -0700

    Add basic actions

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,27 @@
 class SongsController < ApplicationController
+  before_action :set_song!, only: [:show, :edit, :update]
+
+  def index
+    @songs = Song.all
+    render "index"
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  private
+
+  def song_params
+    params.permit(:name, :title, :released, :release_year, :artist_name, :genre)
+  end
+
+  def set_song!
+    @song = Song.find(params[:id])
+  end
 end
