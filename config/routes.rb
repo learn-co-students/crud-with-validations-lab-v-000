9414b0e8ea1cfd9c38b82aa257465127ee0ac552
bmolina-nyc <bruce.d.molina@gmail.com>
2016-10-07 15:27:05 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :songs, only: [:index, :show, :new, :create, :edit, :update, :destroy]
 
-  delete 'songs/:id', to: 'songs#destroy'
+  
+  root "songs#index"
 end
