2c91dce6da62db9b1091613a4ff6f175288ee34c
bmolina-nyc <bruce.d.molina@gmail.com>
2016-10-07 12:33:55 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :songs, only: [:index, :show, :new, :create, :edit, :update, :destroy]
+  get "/songs", to: "songs#index"
+  get "/songs/new", to: "songs#new", as: "new_song"
+  post "/songs", to: "songs#create"
+  get "/songs/:id", to: "songs#show", as: "song"
+  get "/songs/:id/edit", to: "songs#edit", as: "edit_song"
+  patch "/songs/:id", to: "songs#update"
+  delete "/songs/:id", to: "songs#destroy"
 
-  delete 'songs/:id', to: 'songs#destroy'
-
-    root "songs#index"
+  root "songs#index"
 end
