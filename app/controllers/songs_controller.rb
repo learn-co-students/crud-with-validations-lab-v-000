00a70f1208d605d31d180fc0ab11d478e882b0c8
bryan <bryan@snowboardtechie.com>
2016-11-01 14:49:16 -0600

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,51 @@
 class SongsController < ApplicationController
+  before_action :set_song!, only: [:show, :edit, :update]
+
   def index
     @songs = Song.all
   end
 
-  def show
-    @song = Song.find(params[:id])
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
   end
 
   def edit
+  end
+
+  def update
+    @song.update(song_params)
+    if @song.valid?
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Song.find(params[:id]).destroy
+    redirect_to songs_path
+  end
+
+  def show
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :artist_name, :genre, :released, :release_year)
+  end
+
+  def set_song!
     @song = Song.find(params[:id])
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,16 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
+  validates :artist_name, presence: true
+  validates :title, uniqueness: { scope: :release_year, message: "Only one song with a title per year."}
+  validate :release_year_if_released
+
+
+  def release_year_if_released
+    if released && release_year.nil?
+      errors.add(:release_year, 'Release year must be completed if released')
+    elsif released && release_year > Time.now.year
+      errors.add(:release_year, "Release can't be in the future")
+    end
+  end
+
 end
