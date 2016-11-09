a4717a33f0e3a190ffc238eb4181051a5fd9cdc1
Will Lynch <will.lynch91@gmail.com>
2016-11-09 07:21:29 -0500

    got all errors set

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,6 +5,9 @@ class MyValidator < ActiveModel::Validator
                 unless (record.release_year <= Time.now.year.to_i)
                     record.errors[:release_year] << "Release year can't be in the future"
                 end
+                unless Song.all.none? { |s| s.release_year == record.release_year && s.title == record.title  }
+                    record.errors[:release_year] << "Can't release two of the same songs in a year"
+                end
             else
                 record.errors[:release_year] << "Release year can't be empty"
             end
