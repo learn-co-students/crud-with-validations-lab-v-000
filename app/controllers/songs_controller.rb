59ab4c22fcc1504e03c62313923d707996d56d4b
Will Lynch <will.lynch91@gmail.com>
2016-11-09 14:30:04 -0500

    done

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -28,13 +28,18 @@ class SongsController < ApplicationController
 
     def update
         @song = Song.find(params[:id])
-        @song.update(song_params)
+        @song.assign_attributes(song_params)
         if @song.valid?
             @song.save
             redirect_to song_path(@song)
         else
             render :edit
         end
+    end
+
+    def destroy
+        Song.find(params[:id]).destroy
+        redirect_to songs_path
     end
 
     private
