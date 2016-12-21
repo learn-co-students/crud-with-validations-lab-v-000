a68681d64990b2925dce36583fea83d4a93ad263
Baruch Steinmetz <boruchsteinmetz@outlook.com>
2016-12-21 16:32:11 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,21 +1,39 @@
 class SongsController < ApplicationController
 
   def index
+    @songs = Song.all
   end
 
   def show
+    @song = Song.find_by(id: params[:id])
   end
 
   def new
   end
 
   def create
+    @song = Song.create(params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre))
+    if @song.valid?
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
   end
 
   def edit
+    @song = Song.find_by(id: params[:id])
   end
 
   def update
+    @song = Song.find_by(id: params[:id])
+    @song.update(params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre))
+
+    if @song.valid?
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+
   end
-  
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,5 +3,5 @@ class Song < ActiveRecord::Base
   validates :title, uniqueness: {scope: :release_year}
 
   validates :release_year, numericality: { less_than_or_equal_to: Time.now.year.to_i }, presence: true, if: "released?"
-  validates :artist_name: presence: true
+  validates :artist_name, presence: true
 end
