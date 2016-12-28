59946063e790828d931a93a086dfcbf28300d3fd
William Story <wstory@villanova.edu>
2016-12-28 23:56:08 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,15 +1,11 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :artist_name, presence: true
-  validates(:release_year, { :numericality => { :less_than_or_equal_to => Date.today.year}})
-  validate :release_year_no_release
+  validates :release_year, uniqueness: true
 
-  def release_year_no_release
-    if self.released == true
-      unless self.release_year != nil
-        self.errors[:release_year] << "When was it Released?"
-      end
-    end
+  with_options if: :released do |song|
+    song.validates :release_year, presence: true
+    song.validates(:release_year, { :numericality => { :less_than_or_equal_to => Date.today.year}})
   end
 
 end
