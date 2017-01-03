e4721e057b2d23094916e75c71f7cd896b5fdf7c
christinechoi <smi77y@gmail.com>
2017-01-02 23:06:13 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,22 @@ class Song < ActiveRecord::Base
   validates :artist_name, presence: true
   
 
-  validates :release_year, numericality: { less_than_or_equal_to: Time.new.year }
+  #validates :release_year, only_integer: true
 
+  with_options if: :is_released? do |song|
+    song.validates :release_year, presence: true, if: 
+    song.validates :release_year, numericality: { only_integer: true, less_than_or_equal_to: Date.current.year }
+  end
+
+  def is_released?
+    released == true
+  end
+
+
+
+  # if released:
+  # validates :release_year, presence: true, if: :released?
+  # validates :release_year, numericality: { only_integer: true, less_than_or_equal_to: Date.current.year }
+  # else
 
 end
