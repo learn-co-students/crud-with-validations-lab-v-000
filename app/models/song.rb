924d88edfa940a43d82c9ecdb32a768d60a9263b
github username pianistps <github email address pianistps@yahoo.com>
2017-01-26 21:47:25 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,12 +4,12 @@ class Song < ActiveRecord::Base
     scope: [:release_year, :artist_name],
     message: "can't be repeated by the same artist in the same year"
   }
-  validates :released, exclusion: { in: [true, false]}
+  validates :released, inclusion: { in: [true, false]}
   validates :release_year, presence: true, if: :released_year_op
   validates :artist_name, presence: true
 
   def released_year_op
-    if :released == true
+    if released == true
       true
     else
       false
