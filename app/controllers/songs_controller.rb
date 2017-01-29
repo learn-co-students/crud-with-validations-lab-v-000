fff8390182c3188d7d9df017e05d4c893ab58072
Malind Kowalski <malind.nikulski@gmail.com>
2017-01-29 15:25:31 -0700

    completed crud w/ validations

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,48 @@
+class SongsController < ApplicationController
+  before_filter :load_song, only: [:show, :edit, :update]
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @song.update(song_params)
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    load_song.destroy
+    redirect_to songs_path
+  end
+
+  private
+  def load_song
+    @song = Song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+  end
+end
