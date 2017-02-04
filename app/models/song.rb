712f3b96fb175b6bd1c7bd4aae368041db7b4074
Tayari Appiah <cjhhh.ta@gmail.com>
2017-02-04 22:46:55 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,16 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: true
+  validates :released, inclusion: { in: [ true, false ] }
+  validates_presence_of :release_year, :if => :confirmed_release?
+  validates_numericality_of :release_year, :only_integer => true, :less_than_or_equal_to => DateTime.now.year  
+  validates :artist_name, presence: true
+  validates :genre, presence: true
+
+
+# less_than_or_equal_to: DateTime.now.year 
+
+  def confirmed_release?
+    released
+  end 
+
+end
