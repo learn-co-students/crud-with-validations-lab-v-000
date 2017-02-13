e62cafdc31264089357fdf5b3fcfe0805ed101f9
Gordon Siu <gs1@MacBook-Air.local>
2017-02-12 22:21:44 -0800

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,21 +1,32 @@
 class SongsController < ApplicationController
-     before_action :set_song!, only: [:show, :edit, :update]
+     before_action :set_song!, only: [:show, :edit, :update, :destroy]
 
     def index
+        @songs = Song.all
     end
 
     def show
     end
 
+    def new
+        @song = Song.new
+    end
+
     def create
+        @song = Song.new(song_params(:title, :released, :release_year, :artist_name, :genre))
+        if @song.valid?
+          @song.save
+          redirect_to song_path(@song)
+        else
+          render :new
+        end
     end
 
     def edit
     end
 
     def update
-      @song.update(song_params)
-      if @song.valid?
+      if @song.update(song_params(:title, :released, :release_year, :artist_name, :genre))
         redirect_to song_path(@song)
       else
         render :edit
@@ -23,12 +34,14 @@ class SongsController < ApplicationController
     end
 
     def destroy
+        @song.destroy
+        redirect_to songs_path
     end
 
   private
 
-    def song_params
-      params.permit(:title, :released, :release_year, :artist_name, :genre)
+    def song_params(*args)
+        params.require(:song).permit(*args)
     end
 
     def set_song!
