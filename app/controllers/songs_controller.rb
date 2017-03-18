2591df6f29dfcc57e78dcd853d0c1d47c9a472be
Christina L. Cole <christinalcole@gmail.com>
2017-03-18 18:37:54 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -42,6 +42,6 @@ class SongsController < ApplicationController
 
   private
   def song_params
-    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre))
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
   end
 end
