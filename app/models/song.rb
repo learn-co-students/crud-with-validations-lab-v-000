957f3d8c58ca7e76a21f3ae384d866abb2cebe4d
John Pfingst <jpfingst@outlook.com>
2017-03-21 22:18:09 +0000

    add some extra functionality for future reference

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :title, uniqueness: { scope: [:artist_name, :release_year] }
+  validates :title, uniqueness: { scope: [:artist_name, :release_year], message: "must be unique for this artist and this year" }
   validates :artist_name, presence: true
   validates :released, inclusion: { in: [true, false], message: "%{value} is not a valid value"  }
   validate :validate_release_year
