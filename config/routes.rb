fecd707f362229907ec4f10070cbec931062249b
brownielox <mrsmpython@gmail.com>
2017-03-22 15:10:03 -0400

    update delete 2

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
-  get '/', to: 'songs#index'
-  resources :songs
+  get "/songs", to: "songs#index"
+  get "/songs/new", to: "songs#new", as: "new_song"
+  post "/songs", to: "songs#create"
+  get "/songs/:id", to: "songs#show", as: "song"
+  get "/songs/:id/edit", to: "songs#edit", as: "edit_song"
+  patch "/songs/:id", to: "songs#update"
+  delete "/songs/:id", to: "songs#destroy"
+
+  root "songs#new"
+
+  # resources :songs
 end
