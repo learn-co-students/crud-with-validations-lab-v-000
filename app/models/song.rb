ff6f364212266ac99810ff979efd688691aa6ff4
Howard DeVennish <howarddevennish@gmail.com>
2017-04-11 01:55:24 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,7 +2,7 @@ class Song < ActiveRecord::Base
   validates :title, presence: true
   # cannot be repeated by the same artist in the same year
   validates :released, inclusion: {in: [true, false]}
-  valdiates :release_year, numericality: { only_integer: true}
+  validates :release_year, numericality: { only_integer: true}
   #release year is optional if released is false
   #must not be blank if released is true
   #must be less than or equal to current year
