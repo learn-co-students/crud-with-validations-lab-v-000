182f0e85f1777a4c1ee335980e247ae0778f1bfe
Daniel Lempesis <danlrcn@hotmail.com>
2017-04-22 03:48:51 +0000

    Done.

---
+++ db/migrate/20170421211817_create_songs.rb
@@ -0,0 +1,11 @@
+class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.boolean :released
+      t.integer :release_year
+      t.string :artist_name
+      t.string :genre
+    end
+  end
+end
