027b3b337482bc7a2618fad3994777a5c31c20e2
Omar Mason <mr.omarmason@gmail.com>
2017-05-07 18:57:29 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,51 @@
 class SongsController < ApplicationController
+  def show
+    @song = Song.find(params[:id])
+  end
+  def new
+    @song=Song.new
+  end
+
+  def index
+    @songs = Song.all
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def destroy
+     Song.find(params[:id]).destroy
+     redirect_to songs_path
+  end
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+  end
+
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,19 @@
 class Song < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: { scope: :release_year }
+  validates :released, inclusion: { in: [true, false]}
+  validates :artist_name, presence: true
+  validate :pushed_back
+  validate :cater_v
+
+  def pushed_back
+    if release_year && release_year > Time.now.year
+      errors.add(:release_year, "release year cannot be greater than current year.")
+    end
+  end
+
+  def cater_v
+    if released && !release_year
+      errors.add(:release_year, "Year is required for a released song.")
+    end
+  end
 end
