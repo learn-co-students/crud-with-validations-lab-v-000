df88ccb809f871267db084fa61d23692e6e81409
mmelmer <mikemelmer@gmail.com>
2017-05-09 22:52:30 -0400

    Done.

---
+++ db/migrate/20170505222559_songs.rb
@@ -0,0 +1,11 @@
+class Songs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.boolean :released
+      t.integer :release_year
+      t.string :artist_name
+      t.string :genre
+    end
+  end
+end
