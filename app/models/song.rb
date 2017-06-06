f8f6db332ef3ad05b6224f49f19c73dfad486b52
vicision <duncan.j.mclachlan@gmail.com>
2017-06-06 15:18:36 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,9 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: true
+  validates :released, inclusion: { in: [true, false]}
+  validates :release_year, presence: true, if: :released?
+  validates :release_year, :numericality => {less_than_or_equal_to: Date.today.year}, if: :released?
+  validates :artist_name, presence: true
+  validates :genre, presence: true
+
+end
