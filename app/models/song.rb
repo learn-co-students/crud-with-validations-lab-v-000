377ea4d1f445d25f1fbdda35c12eec969205d832
Nick Bair <bairinc@gmail.com>
2017-06-06 19:51:28 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,11 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: :true
+  validates :title, uniqueness: { scope: :release_year,
+    message: "should only happen once per year" }
+  validates :released, inclusion: { in: [true, false] }
+  validates :release_year, presence: :true, if: :released
+  validates :release_year, numericality: {
+    less_than_or_equal_to: Time.now.year
+  }, if: :released
+  validates :artist_name, presence: :true
+end
