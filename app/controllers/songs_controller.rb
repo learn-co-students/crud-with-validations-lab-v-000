ca5423e6ea2ea22e6ff4026889ef65963bab306e
machen2 <victoriamachen@gmail.com>
2017-06-18 16:20:11 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,10 +1,11 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show, :edit, :update, :destroy]
+
   def index
     @songs = Song.all
   end
 
   def show
-    @song = Song.find(params[:id])
   end
 
   def new
@@ -21,11 +22,9 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
   end
 
   def update
-    @song = Song.find(params[:id])
     if @song.update(song_params)
       redirect_to song_path(@song)
     else
@@ -34,12 +33,15 @@ class SongsController < ApplicationController
   end
 
   def destroy
-    @song = Song.find(params[:id])
     @song.destroy
     redirect_to songs_path
   end
 
   private
+    def set_song
+      @song = Song.find(params[:id])
+    end
+
     def song_params
       params.require(:song).permit(:title, :release_year, :released, :genre, :artist_name)
     end
