a18d4b0c5957de63a946406fb37a4c3fc02f6502
Sophia Varella <sophiavarella@gmail.com>
2017-06-30 17:25:43 -0700

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,54 @@
+class SongsController < ApplicationController
+  # before_action :set_song, only: [:show, :edit, :update, :destroy]
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    @song = Song.new(song_params)
+
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params)
+    if @song.valid?
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @song = Song.find(params[:id]).destroy
+    redirect_to songs_path
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+  end
+
+  # def set_song
+  #   @song = Song.find(params[:id])
+  # end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 Rails.application.routes.draw do
+  root to: "songs#index"
+
+  resources :songs
 end
