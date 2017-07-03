bf157e7e5dbf37ecea15cb334c651d4d4420fb42
WilliamBarela <william.barela.dev@gmail.com>
2017-07-03 18:53:30 +0900

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,10 @@
 class Song < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: {scope: [:release_year, :artist_name]}
+  validates :released, inclusion: {in: [true, false]}
+  validates :artist_name, presence: true
+  #validates :release_year, presence: true, if: :released
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+  end
+
 end
