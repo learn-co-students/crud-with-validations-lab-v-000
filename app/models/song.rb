9b4d7b6df35a200d2fba2b490ff2e5a529e1dcb1
Pamela Bortnick <pbortnick@gmail.com>
2017-07-06 19:56:16 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,16 @@
+class Song < ActiveRecord::Base
+
+  validates :title, presence: true
+
+  validates :title, uniqueness: { scope: [:release_year, :artist_name] }
+
+  validates :released,  inclusion: { in: [true, false] }
+
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: { less_than_or_equal_to: Date.current.year }
+  end
+
+  validates :artist_name, presence: true
+
+end
