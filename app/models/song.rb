5e368dec1a17337f2d9d2431ce6bf5953c65c463
Margarita Khodzka <mkhodzka@gmail.com>
2017-08-15 22:34:05 +0000

    added validations to Song

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,10 @@
 class Song < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: true
+  validates :released, inclusion: {in: [true, false]}
+  validates :release_year, presence: true, if: "released?", inclusion: {in: 1..Time.now.year}
+  validates :artist_name, presence: true
+
+  def released?
+    self.released == true
+  end
 end
