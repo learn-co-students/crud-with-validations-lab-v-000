32f1ef20aa0281c6474b35e6215e17ace39f36c3
Catherine Cordova <ccordova41@gmail.com>
2017-08-31 23:43:39 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,34 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :artist_name, presence: true
+  validates :release_year, length: {maximum: Date.today.year}
+  validate :has_release_year?
+  validate :same_song_made?
+  validate :release_year_future?
+
+  def has_release_year?
+    if self.released
+      if release_year == nil
+        errors.add(:release_year, "Must have a release date.")
+      end
+    end
+  end
+
+  def same_song_made?
+      @songs = Song.all
+      @songs.each do |song|
+        if song.artist_name == self.artist_name && song.release_year == self.release_year && song.title == self.title
+          errors.add(:title, "Already have a song with same artist & release year.")
+        end
+      end
+    end
+
+    def release_year_future?
+      if release_year
+        if release_year > Date.today.year
+        errors.add(:release_year, "Must not be a date in the future.")
+        end
+      end
+    end
+
+end
