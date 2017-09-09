b6b4cedd40f7a11257a46b3ee6fbf1e453023a32
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-09-09 13:25:47 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,7 +10,7 @@ class Song < ActiveRecord::Base
 
   with_options if: :released? do |song|
     song.validates :release_year, presence: true
-    song.validates :release_year, numericality: { less_than_or_equal_to: Time.now.yeaer
+    song.validates :release_year, numericality: { less_than_or_equal_to: Time.now.year
     }
   end
 
