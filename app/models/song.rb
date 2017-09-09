4747c4191f9d679c2877457e6fc179cfa2426c81
James Hotchkiss <jimbohotchkiss@yahoo.com>
2017-09-09 12:00:25 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,10 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true 
-    
+  validates :artist_name, presence: true
+  validates :title, presence: true
+  validates :release_year, numericality: {greater_than: Time.now.year}
+  # Title cannot be repeated by same artist in the same year
+  #
+  #
+
+
 end
