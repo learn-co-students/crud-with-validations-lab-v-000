fa7916922b8e4277eddb0a3f22da5f1e244408bc
Tyler Gotto <tylergotto17@gmail.com>
2017-09-10 22:12:15 +0000

    Done.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,27 @@
+class PostsController < ApplicationController
+
+  def index
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+end
