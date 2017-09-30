f9638749c3daf7b34565933cdc253eafd2c579d1
charlie lee <ctlee9096@gmail.com>
2017-09-30 23:08:58 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,5 @@
 class SongsController < ApplicationController
+  before_action :set_song!, only: [:show, :edit, :update]
   def index
     @songs = Song.all
   end
@@ -15,20 +16,32 @@ class SongsController < ApplicationController
     @song = Song.new(song_params)
     if @song.valid?
        @song.save
-      redirect_to song_path(@song)
+       redirect_to song_path(@song)
     else
       render :new
     end
+  end
+  def update
+    @song.update(song_params)
+    if @song.valid?
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+  def destroy
+    Song.find(params[:id]).destroy
+    redirect_to songs_path
   end
 end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_song
+    def set_song!
       @song = Song.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def article_params
-      params.require(:song).permit(:title)
+    def song_params
+      params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
     end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,15 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true, uniqueness: { scope: :release_year,
-    message: "should happen once per year" }
-  #validates_uniqueness_of :title, conditions: -> { where.not(status: 'archived') }
-  validates :released, inclusion: { in: [true, false]}
+    validates :title, presence: true, uniqueness: { scope: :release_year,
+      message: "should happen once per year" }
+    #validates_uniqueness_of :title, conditions: -> { where.not(status: 'archived') }
+    validates :released, inclusion: { in: [true, false] }
+    with_options if: :is_released? do |released|
+      released.validates :release_year, presence: true, numericality: { less_than_or_equal_to: Date.current.year}
+    end
 
+    validates :artist_name, presence: true
+
+    def is_released?
+      self.released == true
+    end
 end
