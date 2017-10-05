1bbda21a599b93c604e5313fe4b1843cc820874c
Nicholas D'Amico <nickalan82@icloud.com>
2017-10-05 17:05:30 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,46 @@
 class SongsController < ApplicationController
+	 before_action :set_song!, only: [:show, :edit, :update, :destroy]
+
+	def index	
+		@songs = Song.all
+	end
+
+	def new
+		@song = Song.new
+	end
+
+	def create 		
+		@song = Song.new(params.require(:song).permit(:title, :artist_name, :release_year, :released, :genre))
+		if @song.save
+			redirect_to song_path(@song)
+		else
+			render :new
+		end
+	end
+
+	def show
+	end
+
+	def edit	
+	end
+
+	def update
+		if @song.update(params.require(:song).permit(:title, :artist_name, :release_year, :released, :genre))
+			redirect_to song_path(@song)
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@song.destroy
+    	redirect_to songs_path
+	end
+
+	private
+
+	def set_song!		
+		@song = Song.find(params[:id])		
+	end
+
 end
---
+++ db/seeds.rb
@@ -0,0 +1,15 @@
+Song.create(
+	:title => "Lucky",
+	:artist_name => "Radiohead",
+	:release_year => 1996,
+	:released => true,
+	:genre => "alternative"
+	)
+
+Song.create(
+	:title => "Rabbitheart",
+	:artist_name => "Florence and The Machine",
+	:release_year => 2009,
+	:released => true,
+	:genre => "indie rock"
+	)
