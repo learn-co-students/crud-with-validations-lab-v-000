2f671c19a04f0c2c071ab97053c1f99dd51e7f15
Cynthia Rittenbach <crittenbach@gmail.com>
2017-10-26 23:12:39 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,22 @@
 require 'pry'
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :release_year, uniqueness: true, inclusion: { in: 0..Date.today.year }
+  validate :invalid_without_release_year_when_released_is_true
+  validate :invalid_when_the_release_year_is_in_the_future
+  validates :release_year, uniqueness: true
 
+  def invalid_without_release_year_when_released_is_true
+    if self.released == true && self.release_year == nil
+       errors.add(:release_year, 'needs a release year')
+    end
+  end
 
+  def invalid_when_the_release_year_is_in_the_future
+    if self.release_year != nil
+      if self.release_year > Time.now.year
+      errors.add(:release_year, 'cant be in the future')
+     end
+   end
+  end
 
 end
