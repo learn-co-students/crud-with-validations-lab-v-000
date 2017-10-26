f34ce8cb9b8eac03438528d1f8d77c3431388649
aisayo <aisayo@icloud.com>
2017-10-26 01:38:15 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,49 +1,57 @@
 class SongsController < ApplicationController
+
   def index
     @songs = Song.all
   end
 
   def show
-    @song = Song.find_by(id: params[:id])
+    @song = set_song! #Song.find(params[:id])
   end
 
   def edit
-    @song = Song.find_by(id: params[:id])
+    @song = set_song! #Song.find(params[:id])
   end
 
   def update
-    @song = Song.find_by(id: params[:id])
-    @song.update(song_params(:title, :released, :release_year, :artist_name, :genre))
+	  @song = set_song!
+	  @song.update(song_params(:title, :released, :release_year, :artist_name, :genre))
     if @song.valid?
-      redirect_to song_path(@song)
+	     redirect_to song_path(@song)
     else
       render :edit
     end
-  end
-
-    def destroy
-      @song = Song.find_by(id: params[:id])
-      @song.destroy
-      redirect_to songs_url
-    end
+	end
 
   def new
     @song = Song.new
   end
 
   def create
-    @song = Song.new(song_params(:title, :released, :release_year, :genre, :artist_name))
+    @song = Song.new(song_params(:title, :released, :release_year, :artist_name, :genre))
     if @song.valid?
-       @song.save
-       redirect_to song_path(@song)
-     else
-       render :new
-     end
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    set_song!.destroy
+    redirect_to songs_url
   end
 
   private
 
-    def song_params(*args)
+  def song_params(*args)
+    #binding.pry
+    #if params.require(:song).permit(:released) == {"released" => true}
       params.require(:song).permit(*args)
-    end
+  #  else
+    #end
+  end
+
+  def set_song!
+    @song = Song.find(params[:id])
+  end
 end
