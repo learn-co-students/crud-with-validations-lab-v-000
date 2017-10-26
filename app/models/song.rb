f46f4232f2f229fa636563bbe6102c9da0e96b98
aisayo <aisayo@icloud.com>
2017-10-26 00:32:17 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,5 +2,6 @@ class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :title, uniqueness: {scope: [:release_year, :artist_name]
   validates :released, inclusion: {in: [true, false]}
+  validates :release_year, 
 
 end
