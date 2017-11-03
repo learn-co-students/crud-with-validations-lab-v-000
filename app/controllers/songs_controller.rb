23c4c7f82e8b330aec6425f7e9b1d937df1012da
lpassamano <leighayna.josele@gmail.com>
2017-11-02 22:11:09 -0400

    added Song controller and view for #new and #create

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -7,7 +7,27 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
   end
 
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
   def edit
     @song = Song.find(params[:id])
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
   end
 end
