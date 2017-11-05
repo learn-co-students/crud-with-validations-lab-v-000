38c2b5ef7844f98ae679e4d0436ff2d6df140937
Lucas <lqf2b8@gmail.com>
2017-11-05 11:08:14 -0600

    done

---
+++ app/models/song.rb
@@ -0,0 +1,11 @@
+class Song < ActiveRecord::Base
+	validates :title, presence: true, uniqueness: true
+	validates :released, inclusion: [true, false]
+	validates :release_year, presence: true, if: "released?", inclusion: {in: 1..Time.now.year}
+	validates :artist_name, presence: true
+	validates :genre, presence: true
+
+	def released?
+		self.released == true
+	end
+end
