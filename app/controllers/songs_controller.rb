8be93cbe3e9b2602f446fa1b9378fc7d3fe9f02d
Christine Tran <christinetran825@gmail.com>
2017-11-13 22:53:29 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,32 @@
 class SongsController < ApplicationController
+
+  def index
+    @song = Songs.all
+  end
+
+  def new
+    @song = Songs.new
+  end
+
+  def create
+    @song = Songs.create
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @song = Songs.find(params[:id])
+  end
+
+  def edit
+    @song = Songs.find(params[:id])
+  end
+
+  def update
+    @song = Songs.find(params[:id])
+  end
+  
 end
