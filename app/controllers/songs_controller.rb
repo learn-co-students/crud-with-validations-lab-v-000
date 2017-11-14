075555718a011682aadf53523ccddcad25a3fb04
Scott Wakeman <github email address>
2017-11-14 12:27:14 -0800

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -55,8 +55,10 @@ class SongsController < ApplicationController
    # DELETE /songs/1.json
    def destroy
      @song.destroy
+     
+
      respond_to do |format|
-       format.html { redirect_to songs_url, notice: 'Song was successfully destroyed.' }
+       format.html { redirect_to songs_path, notice: 'Song was successfully destroyed.' }
        format.json { head :no_content }
      end
    end
