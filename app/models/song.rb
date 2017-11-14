98ae1da8759320692a2775f47c41785398a5beb9
Seb Gertz <sebgertz@gmail.com>
2017-11-14 00:10:08 +0000

    added

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,11 @@
 class Song < ActiveRecord::Base
+  # :scope limits uniqueness check to release_year (check there is at most one song with a given title per release_year)
   validates :title, presence: true, uniqueness: { scope: :release_year }
-  validates :artist_name, presence: true
-  validates :release_year, presence: true, if: :released, numericality: { less_than_or_equal_to: Time.now.year }
   validates :released, inclusion: { in: [ true, false ] }
+  # if :released is truthy, release_year must have a numerical value <= current year
+  validates :release_year, presence: true, if: :released,
+    numericality: { less_than_or_equal_to: Time.now.year }
+  validates :artist_name, presence: true
   validate :check_release_year
 
   def check_release_year
