381c45385ecebe3130e03ec193cd758501eb41d2
jeffrey henderson <jeffrey.henderson17@gmail.com>
2017-11-22 15:46:07 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,9 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, uniqueness: true #test passed but I should make this work more as intended
+  validates :released, inclusion: { in: [ true, false ] }
+
+  validates :release_year, presence: true, if: :released
+  validates :release_year, numericality: { less_than_or_equal_to: Date.today.year}, if: :released
+  validates :artist_name, presence: true
+end
