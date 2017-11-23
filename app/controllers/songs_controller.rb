c1f98caa03559fc0cc4c564d8600009e597846d7
Deborah Seow <deborahseow9@gmail.com>
2017-11-23 07:48:06 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,22 +12,28 @@ class SongsController < ApplicationController
     @song = Song.find_by(id: params[:id])
   end
 
-  def edit
-    @song = Song.find_by(id: params[:id])
-    if @song.update(song_params(:title, :release_year, :artist_name, :genre))
-      redirect_to song_path(@song)
-    else
-      render :edit
-    end
-  end
-
   def create
-    @song = Song.new(song_params(:title, :release_year, :artist_name, :genre))
+    @song = Song.new(song_params(:title, :release_year, :artist_name, :genre, :released))
     if @song.valid?
       @song.save
       redirect_to song_path(@song)
     else
       render :new
+    end
+  end
+
+  def edit
+    @song = Song.find_by(id: params[:id])
+  end
+
+  def update
+    @song = Song.find_by(id: params[:id])
+    # binding.pry
+
+    if @song.update(song_params(:title, :released, :release_year, :artist_name, :genre))
+      redirect_to song_path(@song)
+    else
+      render :edit
     end
   end
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,12 +1,28 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validate :unique_title_in_artist
+  validates :title, uniqueness: {scope: [:artist_name, :release_year],
+  message: "cannot be repeated by the same artist in the same year"}
 
-  def unique_title_in_artist
-    if Song.all.any? do |song|
-      song.artist == :artist_name && song.release_year == :release_year && song.title == :title
-      errors[:title] << "This artist already has a song with this title released in this same year."
-      end
-    end
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {
+      less_than_or_equal_to: Date.today.year
+    }
+  end
+  # include ActiveModel::Validations
+  # validates_with MyValidator, if: :release_year, presence: true
+
+  def released?
+  released
+      # errors[:release_year] << "Release Year can't be blank."
+    # end
   end
 end
+#
+# def validate(record)
+#   if Song.all.any? do |song|
+#     song.artist_name == record.artist_name && song.release_year == record.release_year && song.title == record.title
+#     errors[:base] << "This artist already has a song with this title released in this same year."
+#     end
+#   end
+# end
