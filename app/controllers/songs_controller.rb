de769c1d554f5c5453db1677160e282d3e6ca933
github username <github email address>
2017-11-22 22:07:11 -0500

    finished index and show

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,29 @@
 class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+    get_song
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  private
+
+    def get_song
+      @song = Song.find(params[:id])
+    end
+
 end
