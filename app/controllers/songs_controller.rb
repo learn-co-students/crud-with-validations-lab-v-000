b777562c69edee66cce5c4e939dcd4a802259a5d
github username <github email address>
2017-11-23 09:33:04 -0500

    4 fails left

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,9 +5,16 @@ class SongsController < ApplicationController
   end
 
   def new
+    @song = Song.new
   end
 
   def create
+    @song = Song.new(get_params)
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
   end
 
   def show
@@ -15,15 +22,26 @@ class SongsController < ApplicationController
   end
 
   def edit
+    get_song
   end
 
   def update
+    get_song
+    if @song.update(get_params)
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
   end
 
   private
 
     def get_song
       @song = Song.find(params[:id])
+    end
+
+    def get_params
+      params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
     end
 
 end
