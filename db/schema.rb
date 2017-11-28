3d6275509759d1de147530009f63c57a328e2f3b
dwyn <dwayne.harmon@gmail.com>
2017-11-28 14:24:06 -0600

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171128021821) do
+ActiveRecord::Schema.define(version: 20171128190840) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
     t.boolean  "released"
     t.string   "artist_name"
     t.string   "genre"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.integer  "release_year"
   end
 
 end
