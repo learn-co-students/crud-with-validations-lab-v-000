697cbc48cf5273ad33bad13403431467aefeeb1a
Jessie Huff <huffjd1@gmail.com>
2017-12-05 19:30:41 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,19 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, uniqueness: {
+    scope: [:release_year, :artist_name],
+    message: "artist cannot release the same song in a given year"
+  }
+  validates :artist_name, presence: true
+  validates :released, inclusion: { in: [true, false] }
+  validates :release_year, exclusion: { in: %w(nil) }
+
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {less_than_or_equal_to: Date.today.year}
+  end
+
+  def released? #returns value of released attribute
+    released
+  end
+end
