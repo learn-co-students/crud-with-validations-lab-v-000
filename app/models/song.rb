4a994a494c087b871d59281ef82e81040bd9abba
Adam Tropp <adt6261@gmail.com>
2017-12-13 20:44:08 +0000

    fixed validation

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
   validates_uniqueness_of :title, scope: [:artist_name, :release_year]
-  validates :released, inclusion: { in: %w(true false), message: "%{value} is not a valid option"}
+  validates :released, inclusion: { in: [true, false]}
   validates :release_year, presence: true, if: :released
   validates :release_year, numericality: { less_than: Time.now.year }
   validates :artist_name, presence: true
