d7d6e9096ad3d7ecbc4991794020854848ad29ac
Rodrigo Lodeiro <rodrigo.lodeiro@live.com>
2017-12-14 04:00:55 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,11 @@
+class Song < ActiveRecord::Base
+
+    validates :title, presence: true
+    validates :title, uniqueness: {scope: [:artist_name, :release_year]}
+    validates :released, inclusion: { in: [true, false] }
+    with_options if: :released? do |song|
+        song.validates :release_year, presence: true
+        song.validates :release_year, numericality: {less_than_or_equal_to: Date.today.year}
+    end
+    validates :artist_name, presence: true
+end
