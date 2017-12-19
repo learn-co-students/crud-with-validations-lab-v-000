beb79fc67c86adfa618b597be22dc21dd71af0b7
EC2 Default User <ec2-user@ip-10-0-0-95.ec2.internal>
2017-12-19 15:38:33 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -38,6 +38,12 @@ class SongsController < ApplicationController
     end
   end
 
+  def destroy
+    @song = Song.find(params[:id])
+    @song.destroy
+    redirect_to songs_path
+  end
+
   private
 
   def song_params
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,9 @@
 class Song < ActiveRecord::Base
-    validates :title, presence: true
+    validates :title, presence: true, uniqueness: { scope: :release_year,
+    message: "only one title per year" }
     validates :artist_name, presence: true
     #validate :validate_released
-    validate :validate_unique_title
+    #validate :validate_unique_title
     
     validates :release_year, presence: true, numericality: {less_than_or_equal_to: Date.today.year}, if: :released 
 
@@ -10,9 +11,10 @@ class Song < ActiveRecord::Base
         #errors.add(:released, "must be boolean") unless self.released.is_a?(Boolean)
     #end
     
-    def validate_unique_title
-        if Song.find_by_title_and_release_year(self.title,self.release_year)
-            errors.add(:title, "only one title per year")
-        end
-    end
+    #def validate_unique_title
+        #if Song.find_by_title_and_release_year(self.title,self.release_year)
+        #if Song.find_by title: self.title, release_year: self.release_year, id: !self.id
+            #errors.add(:title, "only one title per year")
+        #end
+    #end
 end
