7ba8fdbc6fc79c607b629939876d8ee6d36443be
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2017-12-26 18:48:49 -0500

    Pass tests for song model

---
+++ app/models/song.rb
@@ -0,0 +1,27 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true #, uniqueness: true#{scope: release_year}
+  #validates :released# , inclusion: { in: %w(true false)}
+  #validates :release_year, numericality: { only_integer: true, less_than_or_equal_to: Time.now.year }
+  validate :title_unique?, :release_year_needed?, :release_year_greater?
+
+  def title_unique?
+    song = Song.find_by(title: self.title)
+    if song && song.release_year == self.release_year
+      errors.add(:title, "Artist cannot release same song in one year.")
+    end
+  end
+
+
+  def release_year_needed?
+    if self.released == true && (self.release_year == nil || self.release_year == "") #!release_year.valid?
+      errors.add(:release_year, "Please add release year")
+    end
+  end
+
+  def release_year_greater?
+    if release_year && release_year > Time.now.year # && (!self.release_year == nil || !self.release_year == "") && 
+      errors.add(:release_year, "Year must be less than or equal to the current year")
+    end
+  end
+
+end
