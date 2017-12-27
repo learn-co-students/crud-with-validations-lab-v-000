258a0b1fbaa61cd159da4294c8405a7faee31914
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2017-12-27 00:52:03 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,6 +9,12 @@ class SongsController < ApplicationController
     end 
     
     def create 
+        @song = Song.new(params)
+        if @song.valid?
+            @song.save
+            redirect_to song_path(@song)
+        else 
+            render :new
     end 
     
     def edit 
