bc8df3bcaf6bec4dc30352cbf42c33901649d446
EC2 Default User <ec2-user@ip-172-31-35-66.us-east-2.compute.internal>
2017-12-27 01:20:26 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,23 +4,42 @@ class SongsController < ApplicationController
         @song = Song.new
     end 
     
+    def show 
+        @song = Song.find(params[:id])
+    end 
+    
     def index 
         @songs = Song.all
     end 
     
     def create 
-        @song = Song.new(params)
+        @song = Song.new(params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre))
         if @song.valid?
             @song.save
             redirect_to song_path(@song)
         else 
             render :new
+        end
     end 
     
     def edit 
+        @song = Song.find(params[:id])
     end 
     
     def update 
-        
+        @song = Song.find(params[:id])
+        @song.update(params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre))
+        if @song.valid?
+            @song.save
+            redirect_to song_path(@song)
+        else 
+            render :edit
+        end
     end 
+    
+    def destroy
+        Song.find(params[:id]).destroy
+        redirect_to songs_path
+    end 
+    
 end
