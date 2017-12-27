2a3b6d97c34a12126f5e9f07f9d69e82a14d0806
breadoliveoilsalt <25881824+breadoliveoilsalt@users.noreply.github.com>
2017-12-27 12:44:59 -0500

    Pass tests for SongsController

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,6 +4,12 @@ class Song < ActiveRecord::Base
   #validates :release_year, numericality: { only_integer: true, less_than_or_equal_to: Time.now.year }
   validate :title_unique?, :release_year_needed?, :release_year_greater?
 
+  # def released_tf?
+  #   if self.released != true || self.released != false
+  #     errors.add(:released, "Must be true or false.")
+  #   end
+  # end
+
   def title_unique?
     song = Song.find_by(title: self.title)
     if song && song.release_year == self.release_year
@@ -19,7 +25,7 @@ class Song < ActiveRecord::Base
   end
 
   def release_year_greater?
-    if release_year && release_year > Time.now.year # && (!self.release_year == nil || !self.release_year == "") && 
+    if release_year && release_year > Time.now.year # && (!self.release_year == nil || !self.release_year == "") &&
       errors.add(:release_year, "Year must be less than or equal to the current year")
     end
   end
