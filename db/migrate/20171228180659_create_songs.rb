04595b3d2178b53a9d9065e0f0eadd926aeb1ddd
dbfarms <matt.hunger@gmail.com>
2017-12-28 19:42:36 +0000

    Done.

---
+++ db/migrate/20171228180659_create_songs.rb
@@ -0,0 +1,14 @@
+class CreateSongs < ActiveRecord::Migration
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.string :artist_name
+      t.string :string
+      t.integer :release_year
+      t.boolean :released
+      t.string :genre
+
+      t.timestamps null: false
+    end
+  end
+end
