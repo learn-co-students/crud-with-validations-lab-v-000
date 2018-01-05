ae6dc8ec8e378ef1b94348255e690c96dba2f07b
Yvonne Dreptate <YvonneDreptate@gmail.com>
2018-01-05 13:36:23 -0500

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,19 @@
+Song.create(title: "Talisman",
+      artist_name: "Air",
+      release_year: 2007,
+      released: true,
+      genre: "Post-Rock")
+
+      Song.create(title: "Song 2",
+            artist_name: "Air trumpet",
+            release_year: 2007,
+            released: true,
+            genre: "Post-Rock")
+
+
+
+            Song.create(title: "Song 3",
+                  artist_name: "Air flute",
+                  release_year: 2007,
+                  released: true, 
+                  genre: "Post-Rock")
