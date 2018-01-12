de1fe916f8d172d3ac7375f218fbe39957d696f0
Eclecticdynastye <bklyngal1994@gmail.com>
2018-01-12 12:57:13 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'songs#index'
+   resources :songs, only: [:index, :show, :new, :create, :edit, :update, :index, :destroy]
+   patch 'songs/:id', to: 'songs#update'
 end
