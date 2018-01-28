2a5250530dae407924543b4721cd78b83694139a
Hannah Carnes <hecarnes@gmail.com>
2018-01-27 23:52:46 -0500

    stopping point

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,30 @@
+class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+    
+  end
+
+  private
+
+  def song_params
+    params.permit(:title, :released, :released_year, :artist_name, :genre)
+  end
+end
