24b8dfb4c03ea9a68873ab698a6c930525799ca6
anginblu <32338385+anginblu@users.noreply.github.com>
2018-01-29 13:27:48 -0500

    views2

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,8 +1,8 @@
 class SongsController < ApplicationController
-  before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :set_song, only: [:show, :edit, :update, :destroy]
 
   def index
-    @articles = Article.all
+    @songs = Song.all
 
   end
 
@@ -11,7 +11,7 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new(article_params)
+    @song = Song.new(title: params[:song][:title], release_year: params[:song][:release_year], released: params[:song][:released], genre: params[:song][:genre], artist_name: params[:song][:artist_name])
 
     if @song.valid?
       @song.save
@@ -23,16 +23,14 @@ class SongsController < ApplicationController
   end
 
   def edit
-    @song = Song.find(params[:id])
   end
 
   def update
-    @song = Song.find(params[:id])
-    @song.title = params[:title]
-    @song.released = params[:released]
-    @song.release_year = params[:release_year]
-    @song.artist_name = params[:artist_name]
-    @song.genre = params[:genre]
+    @song.title = params[:song][:title]
+    @song.released = params[:song][:released]
+    @song.release_year = params[:song][:release_year]
+    @song.artist_name = params[:song][:artist_name]
+    @song.genre = params[:song][:genre]
 
     if @song.valid?
       @song.save
@@ -43,7 +41,6 @@ class SongsController < ApplicationController
   end
 
   def show
-    @song = Song.find(params[:id])
   end
 
   def destroy
@@ -52,16 +49,17 @@ class SongsController < ApplicationController
       format.html { redirect_to songs_url, notice: 'Song was successfully destroyed.' }
       format.json { head :no_content }
     end
+    redirect_to songs_path
   end
 
   private
   # Use callbacks to share common setup or constraints between actions.
-  def set_article
-    @article = Article.find(params[:id])
+  def set_song
+    @song = Song.find(params[:id])
   end
 
   # Never trust parameters from the scary internet, only allow the white list through.
-  def article_params
-    params.require(:title, :released, :release_year).permit(:title, :released, :release_year, :artist_name, :genre)
-  end
+  # def song_params
+  #   params.require(:title, :released, :release_year).permit(:title, :released, :release_year, :artist_name, :genre)
+  # end
 end
