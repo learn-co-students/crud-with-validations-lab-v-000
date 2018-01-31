9365ae767c38dd05069277d81e391c2a56c3d01d
AnastasiaBarkett <anastasia.barkett@gmail.com>
2018-01-30 19:01:39 -0500

    Add validations for Song attributes

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,14 @@ class Song < ActiveRecord::Base
   validates :title, uniqueness: {
     scope: [:release_year, :artist_name]
   }
+
+  validates :released, inclusion: { in: [true, false]}
+  validates :artist_name, presence: true
+
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality:{
+      less_than_or_equal_to: Date.today.year
+    }
+  end
 end
