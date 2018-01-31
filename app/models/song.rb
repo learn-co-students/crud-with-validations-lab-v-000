d84883ae359c661f91ecdde2de329b23c0f54c9b
Andrea Nasuto <andrea.nasuto@gmail.com>
2018-01-31 00:44:06 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,15 @@
 class Song < ActiveRecord::Base
+    validates :title, presence: true, uniqueness: true
+    validates :released, inclusion: { in: [ true, false ] }
+    validates :release_year, presence: true, if: :has_been_released? && :check_release_year
+    validate :check_release_year
+    validates :artist_name, presence: true
+
+ def has_been_released?
+   self.released == true
+ end
+
+ def check_release_year
+   self.release_year <= Time.new.year
+ end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,16 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180130230630) do
+ActiveRecord::Schema.define(version: 20180131000414) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
     t.boolean  "released"
-    t.integer  "released_year"
+    t.integer  "release_year"
     t.string   "artist_name"
     t.string   "genre"
-    t.datetime "created_at",    null: false
-    t.datetime "updated_at",    null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
   end
 
 end
