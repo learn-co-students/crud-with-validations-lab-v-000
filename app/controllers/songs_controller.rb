79052265880a151754c9ad5aedd6f9a177bc35e5
AnastasiaBarkett <anastasia.barkett@gmail.com>
2018-01-31 19:40:57 -0500

    Add new and create routes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -8,4 +8,24 @@ class SongsController < ApplicationController
     @song = Song.find(params[:id])
   end
 
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(
+      :title, :release_year, :released, :genre, :artist_name
+    )
+  end
 end
