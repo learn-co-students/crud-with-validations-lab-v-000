8f8acb83adacaa29ab959f0c71972372f0369f96
AnastasiaBarkett <anastasia.barkett@gmail.com>
2018-01-31 19:50:12 -0500

    Create song destroy route

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -34,6 +34,12 @@ class SongsController < ApplicationController
     end
   end
 
+  def destroy
+     @song = Song.find(params[:id])
+     @song.destroy
+     redirect_to songs_url
+  end
+
   private
 
   def song_params
