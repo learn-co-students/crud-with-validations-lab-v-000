eeed436e476cdc43223f6d42dcef83c07260165f
tpetersen0308 <travis.e.petersen@gmail.com>
2018-02-22 13:17:59 -0800

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,13 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :released, inclusion: {in: [true, false]}
+  validates :release_year, presence: true, numericality: {less_than_or_equal_to: Date.today.year}, if: :released 
+  validates :artist_name, presence: true
+  validate :stale_title?, if: :released
+
+  def stale_title?
+    if self.class.all.any?{|song| song.artist_name == artist_name && song.title == title && song.release_year == release_year}
+      errors.add(:title, "A title cannot be released by the same artist in the same year")
+    end
+  end
+end
