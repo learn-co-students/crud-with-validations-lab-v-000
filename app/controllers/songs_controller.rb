238426c7aa275cc01f262b57460ecd760fdddb10
lsivak <https://github.com/lsivak>
2018-03-03 21:47:10 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,12 +1,22 @@
 class SongsController < ApplicationController
-  
+
  def new
     @song = Song.new
  end
- 
+
  def show
     @song = Song.find(params[:id])
  end
+
+ def create
+    @song = Song.new(song_params)
+  if @song.valid?
+    @song.save
+    redirect_to song_path(@song)
+  else
+    render :new
+  end
+  end
 
   def edit
      @song = Song.find(params[:id])
@@ -21,22 +31,22 @@ class SongsController < ApplicationController
     render :edit
     end
   end
-  
+
   def destroy
       Songs.find(params[:id]).destroy
       redirect_to song_url
   end
 
   def errors_message
-   errors.add(:title, "Title can't be blank") 
+   errors.add(:title, "Title can't be blank")
    errors.add(:released, "Must be True or False")
-   errors.add(:artist_name, "Artist name can't be blank") 
-   errors.add(:release_date, "Must be less than or equal to current year") 
+   errors.add(:artist_name, "Artist name can't be blank")
+   errors.add(:release_date, "Must be less than or equal to current year")
   end
 
   private
 
   def post_params
-    params.permit(:title, :category, :content)
+    params.permit(:title, :released, :release_date, :artist_name)
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,9 @@
 class Song < ActiveRecord::Base
     validates :title, presence: true
-    validates :artist_name, :release_year, uniqueness: true
+    validates :artist_name, uniqueness: true
     validates :release_year, numericality: {
         only_integer: true,
         less_than_or_equal_to: Date.current.year
     }
+    validates :field, inclusion: { in: [ true, false ] }
 end
