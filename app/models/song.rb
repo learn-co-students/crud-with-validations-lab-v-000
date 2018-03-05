f52b2e62ba594b2ccbb376cbf43acc151ce6e312
mbalsamo <github email address>
2018-03-05 13:20:46 -0500

    complete controllers tests

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,7 +6,11 @@ class Song < ActiveRecord::Base
 
   with_options if: :released? do |song|
     song.validates :release_year, presence: true
-    song.validates :release_year, numericality: { less_than_or_equal_to: Date.today.year } 
+    song.validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }
+  end
+
+  def released?
+    released
   end
 
 end
---
+++ db/seeds.rb
@@ -0,0 +1,4 @@
+
+
+
+songs = Song.create([{title: "Lifted Up", released: true, release_year: 2015, artist_name: "Passion Pit", genre: "Alt Rock"}, {title: "Whole Life Story", released: true, release_year: 2016, artist_name: "Passion Pit", genre: "Rock"}])
