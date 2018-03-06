d64db7b04520dd912e46abcc30f7b0d85112e814
Caroline Shields <cbshields@outlook.com>
2018-03-06 23:29:06 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,16 @@
+class Song < ActiveRecord::Base
+  validates :title, :artist_name, presence: true
+  validates :released, inclusion: {in: [true, false]}
+  validates :title, uniqueness: {
+    scope: [:release_year, :artist_name],
+    message: "cannot be repeated by the same artist in the same year"
+  }
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {less_than_or_equal_to: Date.today.year}
+  end
+
+  def released?
+    released
+  end
+end
