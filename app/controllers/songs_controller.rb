f717301ccf7142a81be6e9108bcb9cda611ff1e8
funemployeddad <funemployeddad@gmail.com>
2018-03-11 20:44:35 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,65 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    set_song
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def edit
+    set_song
+  end
+
+  def create
+    @song = Song.new(song_params)
+
+    respond_to do |format|
+      if @song.save
+        format.html {redirect_to @song}
+      else
+        format.html {render :new}
+      end
+    end
+  end
+
+  def update
+    set_song
+    respond_to do |format|
+      if @song.update(song_params)
+        format.html { redirect_to @song, notice: 'Song was successfully updated.' }
+        format.json { render :show, status: :ok, location: @song }
+      else
+        format.html { render :edit }
+        format.json { render json: @song.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    set_song
+    @song.destroy
+    respond_to do |format|
+      format.html { redirect_to songs_url, notice: 'Song was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+
+
+  private
+
+  def set_song
+    @song = Song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+  end
 end
