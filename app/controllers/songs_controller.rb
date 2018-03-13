402e2a7520389e0cf647f658d395d4e3a4d53d7b
Alexander Urizar <urizaraj@gmail.com>
2018-03-13 18:08:31 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,27 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @songs = Song.all
+  end
+
+  def create
+    song = Song.new(strong_params)
+    return render :new unless song.save
+    redirect_to song_path(song)
+  end
+
+  def update
+    return render :edit unless @song.update(strong_params)
+    redirect_to song_path(@song)
+  end
+
+  private
+  def set_song
+    @song = Song.find(params[:id])
+  end
+
+  def strong_params
+    params.require(:song).permit(*%i[title released release_year artist_name genre])
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,10 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :artist_name, presence: true
-  validates :release_year, presence: true,
-                           numericality: { less_than_or_equal_to: Date.today.year },
-                           uniqueness: { scope: :title },
-                           if: :released
+  validates :release_year,
+    presence: true,
+    numericality: { less_than_or_equal_to: Date.today.year },
+    uniqueness: { scope: :title },
+    if: :released
 
 end
