3cdfb3628da37737640262e1f569dd71a06f70ce
jacobsilver2 <jacobsilver2@mac.com>
2018-03-13 18:48:50 -0400

    first commit

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,42 @@
+class SongsController < ApplicationController
+  
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
+  private
+  def song_params
+    params.permit(:title, :released:, :release_year, :artist_name, :genre)
+  end
+
+end
+
+
+
+
+
+# private
+
---
+++ app/models/song.rb
@@ -0,0 +1,8 @@
+class Song < ActiveRecord::Base
+end
+
+
+
+# validates :title, presence: true
+# validates :content, length: {minimum: 100}
+# validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
