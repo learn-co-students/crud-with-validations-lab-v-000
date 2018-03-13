ae0b03b9fa899fcc56390eb1561717303272309f
Alexander Urizar <urizaraj@gmail.com>
2018-03-13 17:39:35 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,9 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :released, presence: true
   validates :artist_name, presence: true
+  validates :release_year, presence: true,
+                           numericality: { less_than_or_equal_to: Date.today.year },
+                           uniqueness: { scope: :title },
+                           if: :released
 
-  with_options if: :released do |song|
-    song.validates :release_year, presence: true,
-                                  numericality: { less_than_or_equal_to: Time.now.year }
-  end
 end
