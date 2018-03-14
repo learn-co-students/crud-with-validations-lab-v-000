8844a46024dfcc3d91f91dfacea0e7656c87c08a
Adjoa Darien <adjoa.darien@gmail.com>
2018-03-14 13:32:54 +0000

    automatically backed up by learn

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+song_list = [
+  {title: "You Should Be Here", released: true, release_year: 2015, artist_name: "Kehlani", genre: "R&B"},
+  {title: "Here We Go", released: true, release_year: 2016, artist_name: "Royal Blu", genre: "Hip Hop"},
+  {title: "Broken Clocks", released: true, release_year: 2017, artist_name: "SZA", genre: "Hip Hop"},
+  {title: "my Gf", released: true, release_year: 2018, artist_name: "Runkus", genre: "Reggae"},
+  {title: "JA", released: true, release_year: 2010, artist_name: "Protoje", genre: "Reggae"}
+]
