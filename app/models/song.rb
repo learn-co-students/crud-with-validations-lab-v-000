48684435893a63ac73e598124b2e71eeabc9f7d3
jacobsilver2 <jacobsilver2@mac.com>
2018-03-14 01:30:20 -0400

    still working on validations

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,11 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true
+  validates :title, uniqueness: {
+    scope: [:artist_name, :release_year], message: "can't be by the same artist in the same year"
+  }
+  validates :released, inclusion: {in: %w(true false), message: "must be true or false"}
+  validates :artist_name, presence: true
+
+
   
 end
 
@@ -8,3 +14,6 @@ end
 # validates :title, presence: true
 # validates :content, length: {minimum: 100}
 # validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+
+# validates :size, inclusion: { in: %w(small medium large),
+#   message: "%{value} is not a valid size" }
