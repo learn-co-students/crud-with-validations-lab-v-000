be2111990f2c949502e7a6137f0df7adf6fbf82b
kostanzhoglo <jeffreywithers@gmail.com>
2018-03-28 21:41:49 -0400

    Add CRUD functionality to Song model

---
+++ app/models/song.rb
@@ -0,0 +1,10 @@
+class Song < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :title, uniqueness: { scope: [:artist_name, :release_year], message: "Song can't be repeated by same artist in same year." }
+  validates :released, inclusion: { in: [true, false] }
+  validates :release_year, presence: true, numericality: { less_than_or_equal_to: Date.today.year },
+    if: :released
+  validates :artist_name, presence: true
+
+end
