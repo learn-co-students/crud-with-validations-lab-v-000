002d1bad973f5c792b3c29dca577dbb5150dcee5
Chris Henry <cdhenry@gmail.com>
2018-03-30 22:35:00 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,7 +27,7 @@ class SongsController < ApplicationController
 
   def update
     @song = Song.find(params[:id])
-
+    
     if @song.update(song_params)
       redirect_to song_path(@song)
     else
