a83f5d6397fa932234f8f0d926e1d86ea7a94c8d
github username <github email address>
2018-04-04 16:51:22 -0400

    Done.

---
+++ db/seeds.rb
@@ -0,0 +1,2 @@
+Song.create(title: "test1", released: true, release_year: 1990, artist_name: "joejoe", genre: "chillin")
+Song.create(title: "test2", released: false, artist_name: "BeanBag", genre: "lazin")
