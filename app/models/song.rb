fad9db63de828dbe98368ecad09d621e92f87019
Katrina Brinson <kbrinso@gmail.com>
2018-04-07 00:10:54 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,9 +4,9 @@ class Song < ActiveRecord::Base
   validates :title, uniqueness: {scope: [:artist_name, :release_year]}
   validates :released, inclusion: { in: [ true, false ] }
   validates :release_year, presence: true, if: :released?
-  validates :release_year, numericality: {less_than_or_equal_to: (Time.now.year)}
+  validates :release_year, numericality: {less_than_or_equal_to: (Time.now.year)}, if: :released?
 
   def released?
-    released == true
+    released
   end
 end
