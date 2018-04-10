a0fdf900e566e9b0d04905ab2df01e00b600df62
Chely Ho <adept_inept@yahoo.com>
2018-04-10 20:49:58 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,34 @@
 class SongsController < ApplicationController
+
+before_action :set_song, only: [:show, :edit, :update, :destroy]
+
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def set_song
+    @song = Song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:title, :released, :released_year, :artist_name, :genre)
+  end
 end
