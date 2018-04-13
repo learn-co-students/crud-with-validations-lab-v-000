f1fd6e3c6b4f8369290d6d224c2098731d1c426d
badlychadly <chadmontoya21@yahoo.com>
2018-04-12 20:42:40 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,53 +1,51 @@
 class SongsController < ApplicationController
-  before_action :set_song, except: [:index, :create, :new]
-
 
   def index
     @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
   end
 
   def new
     @song = Song.new
   end
 
+  def edit
+    @song = Song.find(params[:id])
+  end
+
   def create
     @song = Song.new(song_params)
     if @song.save
-      redirect_to song_path(@song)
-    else  
+      redirect_to @song
+    else
       render :new
     end
   end
 
-  def show
-
-  end
-
-  def edit
-    
-  end
-
   def update
+    @song = Song.find(params[:id])
     if @song.update(song_params)
-      redirect_to song_path(@song)
+      redirect_to @song
     else
       render :edit
     end
   end
 
   def destroy
-    @song.destroy 
-    redirect_to songs_path
+    @song = Song.find(params[:id])
+    @song.destroy
+    redirect_to songs_url
   end
 
   private
 
-  def set_song 
-    @song = Song.find(params[:id])
-  end
-
   def song_params
-    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+    params.require(:song).permit(
+      :title, :release_year, :released, :genre, :artist_name
+    )
   end
 
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,29 +1,20 @@
 class Song < ActiveRecord::Base
-  validates :title, :artist_name, presence: true
-  # validates :release_year, presence: true, if: :was_released?   
-  # validate :valid_release_year?
-  with_options if: :was_released? do |s|
-    s.validates :release_year, presence: true
-    s.validate :valid_release_year?, :released_once_this_year?
+  validates :title, presence: true
+  validates :title, uniqueness: {
+    scope: [:release_year, :artist_name],
+    message: "cannot be repeated by the same artist in the same year"
+  }
+  validates :released, inclusion: { in: [true, false] }
+  validates :artist_name, presence: true
+
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {
+      less_than_or_equal_to: Date.today.year
+    }
   end
 
-
-  def was_released?
-    self.released
-  end
-
-  def valid_release_year?
-    if release_year.present? && release_year > Date.today.year
-      errors.add(:release_year, "released year can't be in the future") 
-    end
-  end
-
-  def released_once_this_year?
-    song = Song.find_by(title: self.title)
-    if !!song
-      if release_year == song.release_year && artist_name == song.artist_name && title == song.title
-        errors.add(:release_year, "cannot release same song twice this year")
-      end 
-    end
+  def released?
+    released
   end
 end
