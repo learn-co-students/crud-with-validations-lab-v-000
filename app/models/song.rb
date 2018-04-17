c26e3766b7b9dcff8360d29255ddb6c4d04b794b
Naomi Dennis <njdennis94@gmail.com>
2018-04-17 23:02:06 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,8 @@
-require_relative "../helpers/song_helper.rb"
 class Song < ActiveRecord::Base
-  include SongHelper
   validates :title, presence: true, uniqueness: true
   validates :artist_name, presence: true
   validates :genre, presence: true
   validates :released, inclusion: {in: [true, false] }
-  validates_presence_of :release_year, if: Proc.new{|obj| obj[:released] == true}
-  validates_inclusion_of :release_year, in: => 0..2018
+  validates_presence_of :release_year, if: Proc.new{|obj| obj[:released] == true}, message: " cannot be set if the song is not released yet!"
+  validates_inclusion_of :release_year, :in => 0..2018, if: Proc.new{|obj| obj[:release_year].present?}, message: " cannot be released in the future!"
 end
