16923bd1448c0273e439cf27483e0b3dca8326fa
Susan Fowler <infopro94@gmail.com>
2018-04-17 11:34:52 -0500

    established validations

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,10 +1,23 @@
 class Song < ActiveRecord::Base
-	validates :title, presence: true # Cannot be repeated by the same artist in the same year
-	validates :released, # Must be true or false
-	validates :release_year # Optional if released is false
-	# Must not be blank if released is true
-	# Must be less than or equal to the current year
+	validates :title, presence: true 
+	validates :title, uniqueness: { #rubyguides section 2.11 validations
+		scope: [:release_year, :artist_name], 
+		message: "cannot be repeated by the same artist in the same year"
+	} 
+
+	validates :released, inclusion: { in: [true, false] } 
 	validates :artist_name, presence: true
-	validates :genre
+
+	with_options if: :released? do |song| #rubyguides section 5.3 conditional validations
+    song.validates :release_year, presence: true 
+    song.validates :release_year, numericality: {
+    	less_than_or_equal_to: Date.today.year
+    }
+
+  end
+
+	def released?
+		released
+	end
 
 end
