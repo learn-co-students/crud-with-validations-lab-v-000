c79a460ed4877480cf5ece7aa66083678162fec2
Ariel Feingold <feingold.ariel@gmail.com>
2018-04-20 14:32:13 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,18 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validate :year_passed?
+  validates :artist_name, uniqueness: { scope: [:release_year, :artist_name],
+    message: "cannot be repeated by the same artist in the same year" }
   validates :released, inclusion: { in: [ true, false ] }
-  validates :release_year, presence: true, if: :released != false
   validates :artist_name, presence: true
 
-  def year_passed?
-    created_at <= 1.year.ago
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {
+      less_than_or_equal_to: Date.today.year
+    }
+  end
+
+  def released?
+    released
   end
 end
