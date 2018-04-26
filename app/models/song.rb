471171c2c284a7e75675f8fab64841607b965ccd
Sean Oughton <seanoughton@gmail.com>
2018-04-26 23:08:39 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,14 +1,19 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
+  #validates :title, uniqueness: {scope: [:release_year, :artist_name]}
+  validates :title, uniqueness: {scope: :release_year}
   validates :artist_name, presence: true
   validates :release_year, presence: true, if: :released?
   validates :released, inclusion: { in: [true, false] }
   validate :future_release_year?
 
+  #invalid if artist has same song in same year
 
   def future_release_year?
-    if release_year > 2018
-      errors.add(:release_year, "must not be in the future")
+    if release_year
+      if release_year > 2018
+        errors.add(:release_year, "must not be in the future")
+      end
     end
   end
 
