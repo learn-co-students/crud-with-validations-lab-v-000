1c88aafdbeff9add29d48e8ec6a35e522681714f
Sean Oughton <seanoughton@gmail.com>
2018-04-26 22:13:45 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -18,6 +18,7 @@ class Song < ActiveRecord::Base
   end #end release_year_check?
 
   def not_released? #returns false if released is false and true if released is true
+    binding.pry
     released == false
   end
 
