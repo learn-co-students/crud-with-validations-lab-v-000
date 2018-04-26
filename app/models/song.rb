7da190f8a70f89d7137da2812f1d554418139b81
Sean Oughton <seanoughton@gmail.com>
2018-04-26 20:14:39 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,5 +3,24 @@ class Song < ActiveRecord::Base
   validates :artist_name, presence: true
   validates :release_year, presence: true
   validates :released, presence: true
+  validate :release_year_check?
+
+  #release_year is optional if released is false
+  #release_year must be less than or equal to the current year
+  binding.pry
+  def release_year_check? #if this returns true the attribute is validated
+    return_value = false
+    if released
+      #if released is true then check to see if the release_year is less than or equal to the current year, if it is then return true
+      if release_year <= 2018
+        return_value = true
+      else
+        return_value = false
+      end
+    else #if released is false return true
+      return_value = true
+    end
+    return_value
+  end #end release_year_check?
 
 end
