ae6e8abdf286a21cbfc9d60dc4b233099eae2fe4
Sean Oughton <seanoughton@gmail.com>
2018-04-26 21:29:50 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,16 +1,20 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :artist_name, presence: true
-  validates :release_year, presence: true
+  #validates :release_year, presence: true
   validates :released, presence: true
   validate :release_year_check?
 
   #song is valid without a release_year when release is false
   def release_year_check?
     if released == true
-      if !release_year #if there is no release year
+      if !release_year #if there is no release year and it has been released
         errors.add(:release_year, "must have a release year")
       end
+    end
+
+    if released == false
+      validates :release_year, presence: false
     end
   end #end release_year_check?
 
