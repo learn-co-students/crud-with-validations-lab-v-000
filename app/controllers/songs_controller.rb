d7f60f17d4dff948f5880f9a82c406a3479ec4c8
Sean Oughton <seanoughton@gmail.com>
2018-04-26 19:25:49 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,13 +1,15 @@
 class SongsController < ApplicationController
 
   def index
-    
+    @songs = Song.all
+    render :index
   end
 
   def new
   end
 
   def show
+    @song = Song.find(params[:id])
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
   root 'songs#index'
   resources :songs
-  #resources :sings, only: [:index, :show, :new, :create, :edit, :update]
 end
