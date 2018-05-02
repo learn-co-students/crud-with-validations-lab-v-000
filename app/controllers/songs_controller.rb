02bea4576648b052cd0ce9370d37e57936ef8fef
Connor Gallopo <connor.gallopo@me.com>
2018-05-02 13:10:52 -0400

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,47 @@
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def destroy
+    Song.find(params[:id]).destroy
+    redirect_to songs_url
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params)
+    if @song.valid?
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+  private
+  def song_params
+      params.require(:song).permit(:title, :artist_name, :genre, :released, :release_year)
+  end
+
+end
