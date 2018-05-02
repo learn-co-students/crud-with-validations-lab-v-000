113b2253c64160ba3729b2995fcd8d048d4ed805
Aubrey Neimeier <neimeier.1@osu.edu>
2018-05-02 18:03:06 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,42 +1,42 @@
-song_class SongsController < ApplicationController
+class SongsController < ApplicationController
   def show
-  @song = Song.find(params[:id])
-end
-
-def new
-  @song = Song.new
-end
-
-def edit
-  @song = Song.find(params[:id])
-end
-
-def create
-  @song = Song.new(song_params)
-
-  if @song.valid?
-    @song.save
-    redirect_to song_path(@song)
-  else
-    render :new
+    @song = Song.find(params[:id])
   end
-end
 
-
-def update
-  @song = Song.find(params[:id])
-
-  if @song.update(song_params)
-    redirect_to song_path(@song)
-  else
-    render :edit
+  def new
+    @song = Song.new
   end
-end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def create
+    @song = Song.new(song_params)
+
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+
+  def update
+    @song = Song.find(params[:id])
+
+    if @song.update(song_params)
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
 
 private
 
 def song_params
-  params.permit(:title, :category, :content)
+  params.permit(:title, :released, :release_year, :artist_name, :genre)
 end
-end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,26 @@
 class Song < ActiveRecord::Base
   validates :title, presence: :true
+  validate :unique_song?
   validates :released, inclusion: { in: %w(true false)}
+  validate :release_info_valid?
   validates :release_year, numericality: { greater_than: 2018}
   validates :artist_name, presence: :true
+
+# custom validations to semantically represent the VALID CASE. for example if a
+# song is valid if it is unique, then name song_unique? and describe valid
+# example, (or invalid example with unless)
+
+  def unique_song?
+    @repeat = Song.where(title: self.title)
+      @repeat.none? do |song|
+        song.release_year == self.release_year && song.artist_name == self.artist_name
+      end
+  end
+
+  def release_info_valid?
+    unless self.released == true && self.release_year == nil
+    end
+  end
+
+
 end
