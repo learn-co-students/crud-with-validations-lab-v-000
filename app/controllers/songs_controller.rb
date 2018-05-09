475fb744a916648a1199c0334fc455db3f76e205
roitblatari <roitblatari@gmail.com>
2018-05-09 14:32:25 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,40 @@
 class SongsController < ApplicationController
-  def songs
-    songs = Song.all
+   before_action :set_song, only: [:show, :edit, :update, :destroy]
+  def index
+    @songs = Song.all
+  end
 
+  def show
+
+  end
+
+  def edit
+    
+  end
+  
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+       redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+  # def update
+
+  # end
+
+  private
+
+  def song_params
+    params.permit(:title, :released, :released, :artist_name, :genre)
+  end
+
+  def set_song
+    @song = Song.find(params[:id])
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
 class Song < ActiveRecord::Base
-  validates :title , uniqueness: { scope: :release_year, message: "should happen once per year" }
+  validates :title, presence: true
+# validates :title, uniqueness: true
+  # validate release_year_conditions
+  # uniqueness: { scope: :release_year, message: "should happen once per year" }
+
+#  def release_year_conditions
+#     if self.released == true && self.release_year == nil
+#       errors.add(:release_year, " must have value, if released is true " )
+#     end
+#   end
 end
