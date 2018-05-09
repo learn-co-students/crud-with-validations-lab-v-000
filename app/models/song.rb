e92e1e230e601cf26af827af26f984c80fcbcf42
ElleeB <laura.trager@gmail.com>
2018-05-09 16:16:52 -0400

    Improved validations

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,35 +1,18 @@
-class Song < ActiveRecord::Base
-  validates :title, presence: true
-  validates :released, inclusion: { in: [ true, false ] }
-  validate :unique_title_for_artist_in_year?
-  validate :valid_release_year
+validates :title, presence: true
+# is invalid if an artist tries to release the same song twice in a year
+validates :title, uniqueness: { scope: [:release_year, :artist_name],
+  message: "cannot be repeated by the same artist in the same year" }
+# released must be true or false
+validates :released, inclusion: { in: [true, false] }
+validates :artist_name, presence: true
 
+with_options if: :released? do |song|
+  # is invalid without release year when released is true
+  song.validates :release_year, presence: true
+  # is invalid when the release year is in the future
+  song.validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }
+end
 
-
-  private
-
-  def unique_title_for_artist_in_year?
-    # is invalid if an artist tries to release the same song twice in a year
-    Song.all.any? do |song|
-      if song.title == title && song.artist_name == artist_name && song.release_year == release_year
-        errors[:base] << "title must be unique for the artist within the year"
-      end
-    end
-  end
-
-  def valid_release_year
-    # is invalid without release year when released is true
-    if
-    released && release_year.nil?
-      errors.add(:release_year, "must be included if song has been released")
-    # is valid without release year when released is false
-    elsif
-    !released && release_year.nil?
-      true
-    # is invalid when the release year is in the future
-    elsif
-    release_year > DateTime.now.year
-      errors.add(:release_year, "cannot be in the future")
-    end
-  end
+def released?
+  released
 end
