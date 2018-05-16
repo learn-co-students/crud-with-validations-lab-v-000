1146e45ccc21213055750431fca4581e2ff06f7c
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-16 12:39:42 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,18 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  #validates :release_year, numericality: { less_than_or_equal_to: :released }
+  #validates :title, uniqueness: true, if: :same_release_year
+  validates :release_year, presence: true, if: :released
   validates :artist_name, presence: true
+  validate :release_year_cannot_be_after_current_year
+  #  validate :title_not_duplicate_in_same_year
+
+  #def not_duplicate_in_same_year
+  #  if title.present?
+  #end
+
+  def release_year_cannot_be_after_current_year
+    if release_year.present? && release_year > Date.today.year
+      errors.add(:release_year, "can't be in the future")
+    end
+  end
 end
