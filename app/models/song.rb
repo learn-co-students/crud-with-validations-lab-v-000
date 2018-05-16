e597f6fdd3b013cec0d4644b61cafc72ef78a983
Kim Foukaris <kimfoukaris@outlook.com>
2018-05-16 14:25:29 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,14 +1,10 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  #validates :title, uniqueness: true, if: :same_release_year
+  validates :title, uniqueness: { scope: :release_year,
+    message: "should happen once per year" }
   validates :release_year, presence: true, if: :released
   validates :artist_name, presence: true
   validate :release_year_cannot_be_after_current_year
-  #  validate :title_not_duplicate_in_same_year
-
-  #def not_duplicate_in_same_year
-  #  if title.present?
-  #end
 
   def release_year_cannot_be_after_current_year
     if release_year.present? && release_year > Date.today.year
