583cfedbeb2a7dc17a88865b14e3f1e8615a44ff
Aly Alidina <aly_alidina@hotmail.com>
2018-05-20 02:50:22 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,10 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true
-  validates :artist_name, presence: true
-  validates :released, presence: true
-  validates :release_year, exclusion: { in: [false] }
+  validates :title, presence: true, uniqueness: { scope: [:artist_name, :release_year]}
+  validates :release_year,presence: true, if: :released?, numericality: { only_integer: true, allow_nil: true, less_than_or_equal_to: Time.now.year }
+
+  def released?
+    released
+  end
 end
 
 
