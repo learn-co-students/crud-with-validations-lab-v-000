9636698d98ac9d87b4ef24fae2804f21deec6dc1
Aly Alidina <aly_alidina@hotmail.com>
2018-05-20 18:59:25 +0000

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,42 @@
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.create(song_params(:title, :artist_name, :release_year, :released))
+    if @song.valid?
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params(:title, :artist_name, :release_year, :released ))
+    redirect_to song_path(@song)
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def delete
+  end
+
+  private
+
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,4 @@
 Rails.application.routes.draw do
+  resources :songs,  only: [:index, :new, :edit, :create, :update, :delete, :show]
+  root to: 'songs#index'
 end
