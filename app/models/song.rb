d5b7fd77d6b88c5657cba9a77f6fa4a979a483ae
Erin Walker <erinb0317@hotmail.com>
2018-05-21 20:42:48 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,10 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, uniqueness: { scope: [:release_year, :artist_name],}
+  validates :release_year, presence: true, if: :released?
+  validates :release_year, numericality: {only_integer: true, allow_nil: true, less_than_or_equal_to: Time.now.year}
+
+  def released?
+    released
+  end
+end
