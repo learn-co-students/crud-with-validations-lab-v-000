6b0b26cded1a9cb6f8e79e85343c27156cd1d691
ryanwevans <ryanwevans@icloud.com>
2018-05-24 16:42:30 -0700

    add Song validations; add routes for RESTful routes

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,26 @@
 class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(params[:song])
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+  end
 end
