d4f08d535a8e71f3bb67be6f26a68734c92922c5
Jonathan Tiede <jon@Jonathans-MacBook-Pro.local>
2018-05-25 13:19:37 -0600

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,6 @@
 Rails.application.routes.draw do
+  resources :songs, only: [:new, :edit, :create, :update, :show, :index, :destroy]
+  # skip_before_filter :verify_authenticity_token, :only => [:destroy]
+  # delete 'songs/:id', to: 'songs#destroy'
+  
 end
