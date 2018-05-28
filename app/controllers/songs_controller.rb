42d573f1a59f1576cbcb66cd475caa1fda73da5b
AspenJames <aspenjames@tqca.org>
2018-05-27 17:57:23 -0700

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,54 @@
+class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    set_song
+  end
+
+  def edit
+    set_song
+  end
+
+  def update
+    set_song
+    @song.update(song_params(:title, :artist_name, :genre))
+    if @song.valid?
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params(:title, :released, :release_year, :artist_name, :genre))
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    set_song
+    @song.destroy
+    redirect_to songs_path
+  end
+
+  private
+
+  def set_song
+    @song = Song.find(params[:id])
+  end
+
+  def song_params(*args)
+    params.require(:song).permit(*args)
+  end
+
+end
