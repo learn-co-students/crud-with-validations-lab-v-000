87f400593e19fa55390d800a4e27a0f3324b7eec
AlexFriedland <friedlandaj@gmail.com>
2018-05-29 12:16:30 +0200

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,11 +12,24 @@ class SongsController < ApplicationController
   end
 
   def edit
+    @song = Song.find(params[:id])
+  end
+
+  def create
     @song = Song.new(song_params)
     if @song.save
       redirect_to @song
     else
       render :new
+    end
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to @song
+    else
+      render :edit
     end
   end
 
@@ -27,9 +40,10 @@ class SongsController < ApplicationController
   end
 
   private
+
   def song_params
     params.require(:song).permit(
       :title, :release_year, :released, :genre, :artist_name
-      )
-    end
+    )
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :songs
 
 
-  //set homepage
+  
   root "songs#index"
 end
