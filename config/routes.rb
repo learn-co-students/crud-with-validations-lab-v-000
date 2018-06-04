e1ca28370c1ad181236b9d772b0aeee4612e307f
github username <github email address>
2018-06-04 14:37:55 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,13 @@
 Rails.application.routes.draw do
+  resources :songs
 end
+
+# generates:
+#   get "/songs" -- index
+#   get "/songs/:id" -- show
+#   get "/songs/new" -- new method
+#   post "/songs" -- create
+#   get "/songs/:id/edit" -- edit method
+#   put "/songs/:id" -- update
+#   patch "/songs/:id" -- update
+#   delete "/songs/:id" -- destroy 
