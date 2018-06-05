deb12b90c00bfeaa3d611ee38bf51af8a427c3a3
ryanwevans <ryanwevans@icloud.com>
2018-06-05 16:18:13 -0700

    create CRUD model, routes, actions, views for Song class with validations

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,11 @@
 class SongsController < ApplicationController
+  before_action :set_song!, only:[:show, :edit, :update]
 
   def index
     @songs = Song.all
   end
 
   def show
-    @song = Song.find(params[:id])
   end
 
   def new
@@ -13,14 +13,39 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new(params[:song])
+    @song = Song.new
+    if @song.update(song_params)
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
   end
 
   def edit
-    @song = Song.find(params[:id])
   end
 
   def update
+    if @song.update(song_params)
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Song.find(params[:id]).destroy
+    redirect_to songs_path
+  end
+
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+  end
+
+  def set_song!
     @song = Song.find(params[:id])
   end
+
 end
