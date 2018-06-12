cbb4057b2988e80fc9018e3ff465cab12c2d1be8
Monica Tamaru Jones <mntamaru@gmail.com>
2018-06-12 07:23:50 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,6 +3,6 @@ class Song < ActiveRecord::Base
   validates :title, uniqueness: {scope: [:artist_name, :release_year]}
   validates :released, inclusion: { in: [true, false] }
   validates :artist_name, presence: true
-  #validates :release_year, presence: true, if: :released
+  
   validates_with SongValidator
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,3 @@
 Rails.application.routes.draw do
-  resources :songs, only: [:new, :create, :show, :edit, :update]
+  resources :songs, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 end
