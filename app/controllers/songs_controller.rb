2eee4abb4f084beaa9774205a9d34c88c88474c3
Nicholas Powers <njpowers7915@gmail.com>
2018-06-30 19:25:09 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,32 @@
 class SongsController < ApplicationController
+    def new
+        @song = Song.new
+    end
 
+    def create
+        @song = Song.new(song_params)
+        @song.save
+        redirect_to song_path(@song)
+    end
+
+    def show
+        @song = Song.find(params[:id])
+    end
+
+    def edit
+        @song = Song.find(params[:id])
+    end
+
+    def update
+        @song = Song.find(params[:id])
+        @song.update(song_params)
+        redirect_to song_path(@song)
+    end
+
+    private
+
+    def song_params
+        params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+    end
 end
     
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,4 @@
 Rails.application.routes.draw do
+    resources :songs, only: [:new, :create, :show, :edit, :update]
+    delete 'songs/:id', to: 'songs#destroy'
 end
