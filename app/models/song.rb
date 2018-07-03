ebeb8d1fbaba4a99802b1204f7e2e5ccfe58d7a4
Joseph Parker <jparker010190@gmail.com>
2018-07-03 00:36:03 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,17 @@
+class Song < ActiveRecord::Base
+  validates_presence_of :title, :artist_name
+    validates :title, uniqueness: {
+      scope: [:release_year, :artist_name]
+    }
+    validates :released, inclusion: { in: [true, false] } # in: optional?
+    with_options if: :released? do |song|
+      song.validates :release_year, presence: true
+      song.validates :release_year, numericality: {
+        less_than_or_equal_to: Date.today.year
+      }
+    end
+
+    def released?
+      released
+    end
+end
