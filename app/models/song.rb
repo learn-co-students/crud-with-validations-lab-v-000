bba783f8ef53d50798a651d75c358084037f85a9
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-10 00:46:59 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,8 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true, uniqueness: {scope: [:artist_name, :release_year]}
+  validates :release_year, presence: true if: :self.released?
+
+
+  def
 
 end
