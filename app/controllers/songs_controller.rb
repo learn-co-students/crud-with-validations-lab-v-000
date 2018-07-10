1deb8b657804997273a73e9055526b952ba00801
Petya Djanovska <petya.djanovska@gmail.com>
2018-07-10 03:30:40 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,50 @@
 class SongsController < ApplicationController
+  before_action :set_song!, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @songs = Song.all
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def edit
+  end
+
+  def update
+    @song.update(song_params)
+    if @song.valid?
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+
+    def destroy
+      @song.destroy
+      redirect_to songs_path
+    end
+  end
+
+  def show
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+private
+  def set_song!
+    @song = Song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,8 +1,13 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true, uniqueness: {scope: [:artist_name, :release_year]}
-  validates :release_year, presence: true if: :self.released?
+  validates :release_year, presence: true,  if: :released?
+  validates_absence_of :release_year, unless: :released?
+  validates :release_year, numericality: {less_than_or_equal_to: :current_year, allow_nil: true}
+  validates :artist_name, presence: true
 
+  def current_year
+    Date.current.year
+  end
 
-  def
 
 end
