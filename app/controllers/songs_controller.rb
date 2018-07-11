70ba879710ad6b90c1b3779e870edc679e3c812b
Karl Amoguis <ktamoguis@gmail.com>
2018-07-11 02:18:23 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -12,7 +12,42 @@ class SongsController < ApplicationController
     @song = Song.new
   end
 
+  def create
+    #binding.pry
+    @song = Song.new(song_params)
+    #binding.pry
+    if @song.save
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
 
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Song.find(params[:id]).destroy
+    redirect_to songs_path
+  end
+
+
+  private
+
+    def song_params
+      params.require(:song).permit(:title, :artist_name, :release_year, :released, :genre)
+    end
 
 
 
