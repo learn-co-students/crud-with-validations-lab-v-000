ab640b13ae1907bfbb2f80ae277c3cc9a13d4beb
ElvinGarcia <dimix181@gmail.com>
2018-07-15 17:23:57 -0400

    added show, index, form documents

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,15 @@
 class SongsController < ApplicationController
+    before_action :songs, only: :index
+  def index
+  end
+
+  def show
+  end
+
+  private
+
+  def songs
+      @songs = Song.all
+  end
+
 end
