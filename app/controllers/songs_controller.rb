ab9817c249361f66c850431ab3c61665ea8a6490
ElvinGarcia <dimix181@gmail.com>
2018-07-16 15:15:30 -0400

    added model validation to song and form template

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,9 +4,30 @@ class SongsController < ApplicationController
   end
 
   def show
+    @song = Song.find_by(params[:id])
   end
 
+  def edit
+    @song = Song.find_by(params[:id])
+    render :form
+  end
+
+    def create
+      @song = Song.new(song_params(:title, :artist_name, :release_year, :released, :genre))
+      if @song.valid?
+        @song.save
+        redirect_to song_path(@song)
+      else
+        render :new
+      end
+
+    end
+
   private
+
+  def song_params(*args)
+    params.require(:song).permit(args)
+  end
 
   def songs
       @songs = Song.all
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,4 @@
 class Song < ActiveRecord::Base
+  validates :title, :artist_name, :genre, presence: true
+  validates :title, uniqueness: true
 end
