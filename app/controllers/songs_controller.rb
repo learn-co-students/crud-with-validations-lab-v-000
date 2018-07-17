cfd6b1a5f0ba7a85662e5dd850cf99b2dcad84c2
ElvinGarcia <dimix181@gmail.com>
2018-07-17 15:14:15 -0400

    song controller test are passing

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,10 +9,10 @@ class SongsController < ApplicationController
 
   def edit
     @song = Song.find_by(params[:id])
-    render :form
+    render :_form
   end
 
-    def create
+     def create
       @song = Song.new(song_params(:title, :artist_name, :release_year, :released, :genre))
       if @song.valid?
         @song.save
@@ -20,7 +20,21 @@ class SongsController < ApplicationController
       else
         render :new
       end
+    end
 
+    def update
+        @song = Song.find_by(params[:id])
+      if  @song.update(song_params(:title, :artist_name, :release_year, :released, :genre))
+        redirect_to song_path(@song)
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @song = Song.find_by(params[:id])
+      @song.destroy
+      redirect_to songs_path
     end
 
   private
