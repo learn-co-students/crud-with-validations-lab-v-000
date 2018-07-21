bd5cf3e8411d96d258f06202e0cca9836f3f81aa
ElvinGarcia <dimix181@gmail.com>
2018-07-21 14:46:44 -0400

    model test passing

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,21 @@
 class Song < ActiveRecord::Base
   validates :title, :artist_name, :genre, presence: true
   validates :title, uniqueness: true
+  validate  :clikbate
+
+    def clikbate
+#binding.pry
+        if (released && release_year) && (Date.today.year > release_year  unless release_year == nil) || (!released && release_year.nil?)
+        else
+              errors[:released] << "does not compute"
+       end
+    end
+#  validates :released, inclusion: {in: ["true","false"]}
+
+   # def validate_year
+   #   if release_year.blank? || Time.now.year < release_year
+   #     errors[:base] << "The release year is invalid"
+   #   end
+   # end
+
 end
