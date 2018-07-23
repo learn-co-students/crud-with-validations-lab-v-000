75a24b095d599237b555393168f6a03a4618ef6b
Suchitra Naik <susanaik@gmail.com>
2018-07-23 23:07:31 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,7 @@
 class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :artist_name, presence: true
+  validates :released, inclusion: {in: [true, false]}
+  validates :release_year, presence: true
+
 end
