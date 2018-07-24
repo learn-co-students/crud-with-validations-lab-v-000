6b93dea7f849c6eab3969bb3c8951e63e35f2ee3
barbara.prusiewicz@gmail.com prusiewicz <barbara.prusiewicz@gmail.com>
2018-07-24 20:52:14 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,13 +2,12 @@ class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :released, inclusion: { in: [ true, false ] }
   validates :artist_name, presence: true
-  validates :release_year, :length => {is: 4 }
   validate :is_good_title?
   validate :is_year_present?
   validate :is_year_valid?
 
   def is_good_title?
-    if Song.where(:title => self.title, :released_year => self.release_year)
+    if Song.where(:title => self.title, :release_year => self.release_year).count > 0
       errors.add(:title, "Title was already released in the same year")
     end
   end
@@ -23,9 +22,10 @@ class Song < ActiveRecord::Base
 
   def is_year_valid?
     # note: 1800 arbitrary choice for minimum year.  Better check would probably be something later
-    binding.pry
-    if self.release_year > Date.today.year || self.release_year < "1800"
-      errors.add(:release_year, "Invalid Year")
+    if self.release_year
+        if self.release_year > Date.today.year || self.release_year < 1800
+          errors.add(:release_year, "Invalid Year")
+        end
     end
   end
 end
