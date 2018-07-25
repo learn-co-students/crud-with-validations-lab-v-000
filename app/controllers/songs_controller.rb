9939d1177c8d3e316a439ff359bff48f11000287
Annabel Wilmerding <annabelwilmerding@gmail.com>
2018-07-25 16:55:26 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,25 @@
 class SongsController < ApplicationController
+  
+  def index
+  end
+  
+  def new 
+  end
+  
+  def create 
+  end
+  
+  def show 
+  end
+  
+  def edit 
+  end 
+  
+  def update 
+  end
+  
+  def delete 
+  end
+  
+  
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,5 +3,5 @@ class Song < ActiveRecord::Base
   validates :title, presence: true, uniqueness: { scope: :release_year, :name }
   validates :released, inclusion: {in: [true, false]}
   validates :release_year, presence: true, unless: Song.new { |a| a.released.blank? }, numericality: { less_than_or_equal_to: Date.today.year }
-  
+  validates :artist_name, presence: true
 end
