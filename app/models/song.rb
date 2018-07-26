bd9cb963ecbf659363b04f7e39538c68834e01bf
swgoodman <spencerwgoodman@icloud.com>
2018-07-25 17:17:17 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -10,8 +10,9 @@ class Song < ActiveRecord::Base
   validates :artist_name, presence: true
 
   with_options if: :released? do |song|
-    song.validates :release_year, presence: true
-    song.validates :release_year, numericality: {less_than_or_equal_to: Date.today.year}
+    if song.validates :release_year, presence: true
+      song.validates :release_year, numericality: {less_than_or_equal_to: Date.today.year, message: "must be a number"}
+    end
   end
 
   private
