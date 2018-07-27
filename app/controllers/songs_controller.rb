d6d82b355d4dab5784819bbf44a0f437dfd783de
taryn yaeger <tsyaeger@gmail.com>
2018-07-27 18:32:50 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,5 @@
 class SongsController < ApplicationController
-  before_action :set_song, only: [:show, :edit, :update, :destroy]
+  # before_action :set_song, only: [:show, :edit, :update, :destroy]
   
   def index 
     @songs = Song.all 
