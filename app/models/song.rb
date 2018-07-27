e8cbac3ac8c1174d7cd1457606a75dd15808186e
taryn yaeger <tsyaeger@gmail.com>
2018-07-27 04:07:42 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,7 +6,7 @@ class Song < ActiveRecord::Base
   validates :released, exclusion: { in: [nil] }
   
   validates :release_year, numericality: { only_integer: true, less_than_or_equal_to: Date.current.year.to_i }
-  validates :release_year, presence: true if 
+  validates :release_year, presence: true, if validates_associated :released
   
   validates :artist_name, presence: true 
   validates :artist_name, format: { with: /\A[a-zA-Z]+\z/ }
