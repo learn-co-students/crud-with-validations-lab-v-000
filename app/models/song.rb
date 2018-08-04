0cad14216be2b2ef564e74b1e0d1d90c45025fad
github username <github email address>
2018-08-04 01:18:50 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,29 @@
+class Song < ActiveRecord::Base
+
+# title, a string
+# Must not be blank
+  validates :title, presence: true
+  # Cannot be repeated by the same artist in the same year
+  validates :title, uniqueness: {
+    scope: [:release_year, :artist_name],
+    message: "cannot be repeated by the same artist artist in the same year"
+  }
+  # released, a boolean describing whether the song was ever officially released.
+  validates :released, inclusion: {in: [true, false]}
+  validates :artist_name, presence: true
+
+  # Optional if released is false
+  # Must not be blank if released is true
+  # Must be less than or equal to the current year
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :released_year, numericality:{
+      less_than_or_equal_to: Date.today.year
+    }
+  end
+
+  def released?
+    released
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 Rails.application.routes.draw do
+
+  get "/songs", to: "songs#index"
+  # get "/songs/new" to: "songs#new", as: "new_song"
+  # post "/songs", to: "songs#create"
+
+  root "songs#index"
 end
