7024055dc3a5d766c48e6f6994398a1af8ebbc4c
Sara Stanton <sara.mpa13@gmail.com>
2018-08-04 21:40:19 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,21 +2,26 @@ class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :released, inclusion: { in: [ true, false] }
   validates :artist_name, presence: true
-  # validate :same_artist_year
+  validate :same_artist_year
   validate :check_release_year
 
-  # def same_artist_year
-  #   if :artist
-  #     errors.add(:title, "Error message")
-  #   end
-  # end
+  def same_artist_year
+    artist_songs = []
+    Song.all.each do |song|
+      if song.artist_name == artist_name
+        artist_songs << song
+      end
+    end
+    if artist_songs.detect{|s| s.release_year == release_year}
+      errors.add(:title, "Error message")
+    end
+  end
 
   def check_release_year
     if released
       if !release_year || release_year == ""
         errors.add(:release_year, "Release year cannot be blank")
-      end
-      if release_year > Date.today.year
+      elsif release_year > Date.today.year
         errors.add(:release_year, "Release year cannot be greater than current year")
       end
     end
