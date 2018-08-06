aea3105095503a0e86c0e23faf93ca7b3900218a
Christopher Wood <cjwood032@gmail.com>
2018-08-06 17:56:04 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,9 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: { scope: :artist_name && :release_year }
+  validates :released, inclusion: { in: [true, false] }
+  validates :release_year, presence: true, if: :song_released?, numericality: { less_than_or_equal_to: Date.current.year }
+  validates :artist_name, presence: true
+  def song_released?
+   released == true
+  end
+end
