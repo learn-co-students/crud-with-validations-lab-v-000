666f1d557bf7f505ed1c4b921f1ab2646183d9f0
Veronica Bustabad <vbustabad@gmail.com>
2018-08-10 02:35:44 +0000

    Done.

--- Gemfile
+++ Gemfile
@@ -7,6 +7,8 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
+gem 'jquery-rails'
+gem 'activesupport'
 
 group :development, :test do
   gem 'capybara'
---
+++ app/models/song.rb
@@ -0,0 +1,16 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: { scope: [:artist_name, :release_year], message: "The title cannot be repeated by the same artist in the same year." }
+  validates :released, inclusion: { in: [true, false] }
+
+  with_options if: :is_the_song_released? do
+    validates :release_year, presence: true
+    validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }
+  end
+
+  validates :artist_name, presence: true
+
+  def is_the_song_released?
+    released
+  end
+
+end
