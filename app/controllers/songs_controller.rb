832588e03aea565187a22ca160ddf422090e0fc7
epic0448 <samuelogundiran@Yahoo.com>
2018-08-13 23:19:58 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,38 @@
 class SongsController < ApplicationController
+
+
+    def new
+     @song = Song.find(params[:id])
+    
+    end
+
+    def show
+        @song = Song.find(params[:id])
+
+    end
+
+    def edit
+        @song = Song.find(params[:id])
+
+    end
+
+
+    def update
+
+    end
+    
+    def create
+
+    end
+  
+    #it wants a destroy action.
+
+
+    private
+
+    def song_params
+        params.permit(:artist_name, :title, :genre,:released, :release_year)
+
+    end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,12 @@
 class Song < ActiveRecord::Base
-    validates :title, presence: unique: true
+    validates :title, presence: true
+    validates :title, uniqueness: true
+
     validates :artist_name, presence: true
-    validates :category, inclusion: { in: %w(Fiction Non-Fiction)} #customized 
+    validates :artist_name, uniqueness: true
+
+    # validates :released, #give true or false
+    # validates :released_year, uniqueness: true
+    # validates :category, inclusion: { in: %w(Fiction Non-Fiction)} #customized 
 
 end
