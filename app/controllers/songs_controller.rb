50ba690fd1a7e6f1519cd461621840189e4cd9ba
epic0448 <samuelogundiran@Yahoo.com>
2018-08-14 13:33:02 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,29 +1,41 @@
 class SongsController < ApplicationController
 
 
+    def index
+        @songs = Song.all
+    end
+
     def new
+    
      @song = Song.find(params[:id])
     
     end
 
     def show
         @song = Song.find(params[:id])
-
     end
 
     def edit
         @song = Song.find(params[:id])
-
     end
 
 
     def update
-
+        @song = Song.find(params[:id])
+        @song.update(song_params)
+        redirect_to song_path(@song)
     end
     
     def create
-
+        @song = Song.new(song_params)
+        #  if @song.valid?  #true
+        #   @song.save
+        #  redirect_to song_path(@song)
+        # else
+           render :new
+        # end
     end
+    
   
     #it wants a destroy action.
 
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,11 +1,9 @@
 class Song < ActiveRecord::Base
     validates :title, presence: true
     validates :title, uniqueness: true
-
     validates :artist_name, presence: true
     validates :artist_name, uniqueness: true
-
-    # validates :released, #give true or false
+    validates :released, inclusion: { in: [ true, false ] }
     # validates :released_year, uniqueness: true
     # validates :category, inclusion: { in: %w(Fiction Non-Fiction)} #customized 
 
