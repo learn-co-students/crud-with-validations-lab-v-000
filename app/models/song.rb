6c69bc7412e9cbda069804badd4821ec07610e04
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-18 00:46:48 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,9 @@
+
 class Song < ActiveRecord::Base
+  validates :released, inclusion: { in: [true, false] }
+  validates :title, presence: true
+  validates :artist_name, presence: true
+  
+  
+  
 end
