c827c3d6207463dea0f540222a782670a1c98dd1
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-20 19:29:29 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,18 +2,13 @@
 class Song < ActiveRecord::Base
   
   validates :title, presence: true, uniqueness: {
-    scope: [:release_year, :artist_name]
+    scope: [:artist_name, :release_year]
   }
-  
-  #validate :unique_title_per_artist_per_year
-  # Cannot be repeated by the same artist in the same year
   
   validates :released, inclusion: { in: [true, false] }
   
-  validates :release_year, numericality: { only_integer: true } # Optional if released is false, Must not be blank if released is true, Must be less than or equal to the current year
+  validates :release_year, presence: true, if: -> { :released && :release_year <= DateTime.now.year }  
   
   validates :artist_name, presence: true
   
-  private
-
 end
