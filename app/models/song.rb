6e62083faccdbaaf5863a9d4c77108cf97eba7a7
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-20 16:06:45 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,9 +1,12 @@
 
 class Song < ActiveRecord::Base
+  
+  validates :title, presence: true # Cannot be repeated by the same artist in the same year
+  
   validates :released, inclusion: { in: [true, false] }
-  validates :title, presence: true
+  
+  validates :release_year, numericality: { only_integer: true } # Optional if released is false, Must not be blank if released is true, Must be less than or equal to the current year
+  
   validates :artist_name, presence: true
-  
-  
   
 end
