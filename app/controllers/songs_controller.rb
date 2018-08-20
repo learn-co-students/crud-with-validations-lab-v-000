569fffad625a579f0112590331f4e462b85ab734
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-20 21:33:27 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,29 @@
 
 class SongsController < ApplicationController
+  before_action :find_song, only: [:show, :edit, :update]
+  
+  def index
+    @songs = Song.all 
+  end 
+  
+  def show
+  end
+  
+  def new
+    @song = Song.new
+  end
+  
+  def create
+    @song = Song.new(song_params(:title, :released, :release_year, :artist_name))
+    @song.save
+    redirect_to song_path(@song)
+  end
+  
+  def edit 
+  end
+  
+  def update
+    @song.update()
+  end
   
 end
