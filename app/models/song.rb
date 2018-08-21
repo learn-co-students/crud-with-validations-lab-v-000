6cf21641c66e37c570bad43d79e89fb2eba4b27b
Brennan Fulmer <brennan.flmr@gmail.com>
2018-08-21 01:15:32 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,19 +5,22 @@ class Song < ActiveRecord::Base
     scope: [:artist_name, :release_year]
   }
   
-  validates :released, inclusion: { in: [true, false] }
-  
-  validates :release_year, presence: true, if: -> { released } 
-  
   validates :artist_name, presence: true
   
+  validates :released, inclusion: { in: [true, false] }
+  
+  validates :release_year, presence: true, if: :released? 
   validate :no_future_release
   
   private
     
+    def released?
+      released
+    end
+    
     def no_future_release
       if release_year
-        unless release_year <= DateTime.now.year }
+        unless release_year <= DateTime.now.year
           errors.add(:release_year, "Songs can't be released in the future, they can only be unreleased!")
         end
       end
