ea10d0bb521355f4a5ba73ffc80bdfd8d9d1ef49
Adam Kalt <adam.kalt.1@gmail.com>
2018-08-29 22:43:41 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,12 @@
 class Song < ActiveRecord::Base
+
+validates :title, presence: true
+validates :title, uniqueness: {
+  scope: %i[release_year artist_name],
+  message: 'cannot be repeated by the same artist in the same year'
+}
+validates :released, inclusion: { in: [true, false] }
+validates :artist_name, presence: true
+
+
 end
