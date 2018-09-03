64bcf4436bdb225b778e5b66358aec7fe31daa5e
Josh Cesana <joshuacesana@gmail.com>
2018-09-03 00:05:35 -0700

    Done

--- db/migrate/20180903032646_add_unique_index_to_songs.rb
+++
@@ -1,6 +0,0 @@
-class AddUniqueIndexToSongs < ActiveRecord::Migration
-  def change
-    add_index :songs, :artist_name, unique: true
-    add_index :songs, :release_year, unique: true
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180903032646) do
+ActiveRecord::Schema.define(version: 20180903065325) do
 
   create_table "songs", force: :cascade do |t|
     t.string   "title"
@@ -22,8 +22,5 @@ ActiveRecord::Schema.define(version: 20180903032646) do
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
   end
-
-  add_index "songs", ["artist_name"], name: "index_songs_on_artist_name", unique: true
-  add_index "songs", ["release_year"], name: "index_songs_on_release_year", unique: true
 
 end
