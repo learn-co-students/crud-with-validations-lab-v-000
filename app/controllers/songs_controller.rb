947c797831f6e7afe9cd5ccba7513adab96300d8
Josh Cesana <joshuacesana@gmail.com>
2018-09-02 23:19:21 -0700

    wip

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,27 @@
 class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+  end
 end
