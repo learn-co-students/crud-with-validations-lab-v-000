441c4c97dad20ff354bdf5a5c181da3a6e010167
Christopher P. Dundon <chris.dundon@gmail.com>
2018-09-06 17:59:22 -0400

    improved lab

--- app/models/good_year_validator.rb
+++
@@ -1,9 +0,0 @@
-class GoodYearValidator < ActiveModel::Validator
-  def validate(record)
-    if record.release_year && record.released
-			if (record.release_year > Date.current.year)
-				record.errors[:base] << "The release year must be less than or equal to the current year"
-			end
-		end
-  end
-end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,23 +3,5 @@ class Song < ActiveRecord::Base
 	validates :artist_name, presence: true
 	validates :released, inclusion: { in: [true, false] }
 	validates :title, uniqueness: { scope: [:release_year, :artist_name], message: "should happen once per year per artist name" }
-	validates :release_year, presence: true, if: :released
-	validates_with GoodYearValidator
-
-#	validates :release_year, presence: false, if: :bad_year?
-=begin
-	validates :release_year, numericality: { 
-		only_integer: false, 
-		less_than_or_equal_to: ->(_song) { Date.current.year }
-	}
-=end
-
-	def good_year?
-		if release_year && released
-			#binding.pry
-			(release_year <= Date.current.year)
-		else
-			false
-		end
-	end
+	validates :release_year, presence: true, if: :released, numericality: { less_than_or_equal_to: Date.current.year, only_integer: true }
 end
