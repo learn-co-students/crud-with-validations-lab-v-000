5e856d8fa56821bd9f3c3eaf21da9a29a1a42373
Jonathan Hong <jonhong777@gmail.com>
2018-09-08 15:41:41 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,9 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :released, inclusion: {in: %w(true false)}
+  validates :artist_name, presence: true
+
+  def released?
+    released == true
+  end
+end
