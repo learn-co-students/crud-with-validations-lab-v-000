a408fd0e90ee7549d8a702139e3f887b707b29ed
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-09-10 19:00:16 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,6 @@
 class Song < ActiveRecord::Base
+  validates :title, uniqueness: { scope: :release_year}
+  validates_presence_of :title
+  validates_presence_of :release_year, if: :released?
+  validates_numericality_of :release_year, less_than_or_equal_to: Date.today.year, if: :released?
 end
