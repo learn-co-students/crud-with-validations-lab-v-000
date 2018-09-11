b082a951b35e87fc2f6546dc6e24ea9c8d735668
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-09-11 03:46:39 +0000

    included edit and update controller actions

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -13,7 +13,28 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song = Song.new()
+    @song = Song.new(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song = Song.update(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
   end
 
   private
