c65c02b4cfc672678215688d4a416d8fb429f6c0
Jamilya Ramos-Chapman <jamilya_ramos-chapman@alumni.brown.edu>
2018-09-11 03:36:06 +0000

    set up songs controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,24 @@
 class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new()
+  end
+
+  private
+
+  def song_params
+    params.permit(:title, :released, :release_year, :artist_name, :genre)
+  end
 end
