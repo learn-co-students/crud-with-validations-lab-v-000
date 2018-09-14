0f8bca916adf2cae0d8886b996f4d31691688a10
anikonchuk <anikonch@gmail.com>
2018-09-14 15:34:39 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,23 @@
 class Song < ActiveRecord::Base
+  validates :title, :artist_name, presence: true
+  validates :released, inclusion: { in: [true, false] }
+  validates :release_year, presence: true, if: :released?
+  validate :release_date_in_the_past?
+  validate :same_artist_same_year?
+
+  def release_date_in_the_past?
+    if release_year.present? && release_year > Date.today.year
+      errors.add(:release_year, "must be less than or equal to this year")
+    end
+  end
+
+  def released?
+    released == true
+  end
+
+  def same_artist_same_year?
+    if release_year.present? && Song.find_by(title: title, artist_name: artist_name, release_year: release_year)
+      errors.add(:title, "cannot be the same title if the artist and release year are the same")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
+  root to: 'songs#index'
   resources :songs
 end
