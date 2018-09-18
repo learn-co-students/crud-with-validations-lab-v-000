8013c118d464bb5a4521e8ff4985893c4c601bd5
fkmccallion <fkmccallion@gmail.com>
2018-09-18 06:21:38 -0700

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,4 +5,16 @@ class SongsController < ApplicationController
     def new
         @song = Song.new
     end
+
+    def create
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
 end
