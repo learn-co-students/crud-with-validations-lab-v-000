cbfdc1339b395d8f721e497aba9ca3d5c0410624
Brett Weisberg <weisberg.brett@gmail.com>
2018-09-20 01:46:56 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,22 +1,50 @@
 class SongsController < ApplicationController
 
-
   def new
+    @song = Song.new
   end
 
   def create
+    @song = Song.new(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
+  end
+
+  def show
+      @song = Song.find(params[:id])
   end
 
   def index
+    @songs = Song.all
   end
 
   def edit
-
+    @song = Song.find(params[:id])
   end
 
   def update
+    @song = Song.find(params[:id])
+    if @song.update_attributes(song_params)
+        @song.update(song_params)
+        redirect_to song_path(@song)
+      else
+        render :edit
+      end
   end
 
   def delete
+    Song.find(params[:id]).destroy
+   redirect_to song_url
   end
+
+  private
+
+  def song_params
+    params.permit(:title, :released, :release_year, :artist_name, :genre)
+  end
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,12 @@
 class Song < ActiveRecord::Base
-  validates :name, presence: true
+  validates :title, presence: true, uniqueness: { scope: :release_year}
+  validates :released, inclusion: { in: [true, false] }
+  validates :artist_name, presence: true
+  validates :release_year, length: {is: 4}, if: :is_released?
+
+  def is_released?
+    self.released == true
+  end
+
 
 end
