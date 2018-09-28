de448faa6d7bf5f841e1673235089bdb57cac4ce
Thomas Wesley Matlock <snaxbags@protonmail.com>
2018-09-28 17:53:13 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,15 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates :artist_name, presence: true
+  validates :released, inclusion: { in: [true, false] }
+  validates ;release_year, presence: true, if: :released?
+
+  with_options if: :released? do |song|
+    song.validates :release_year, numericality: {less_than_or_equal_to: Date.today.year}
+  end
+
+  def released?
+    released == true
+  end
+end
