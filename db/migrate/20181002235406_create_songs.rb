fd4f446013191efbb2426e566de05abfc6834dca
Anu Khambete <anukhambete@gmail.com>
2018-10-03 00:03:42 +0000

    Done.

--- db/migrate/20181002235406_create_songs.rb
+++ db/migrate/20181002235406_create_songs.rb
@@ -2,7 +2,7 @@ class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
       t.string :title
-      t.sting :artist_name
+      t.string :artist_name
       t.string :genre
       t.integer :release_year
       t.boolean :released
