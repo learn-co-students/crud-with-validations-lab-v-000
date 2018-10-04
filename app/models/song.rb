2915463f2d3617e9d6f3085241f3f85a6d8067d8
ValerieHMoore <periwinkle17@outlook.com>
2018-10-04 19:14:30 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,20 @@
+class Song < ActiveRecord::Base
+    validates :title, presence: true
+    validates :artist_name, presence: true
+    validates :released, inclusion: { in: [true, false] }
+    validates :title, uniqueness: true, if: :release_year, uniqueness: true
+    validate :is_released
+    validate :current_year
+
+    def current_year
+        if release_year.present? && release_year > Date.today.year
+            errors.add(:release_year, "Can't be in the future")
+        end
+    end
+
+    def is_released
+        if released == true && release_year == nil
+            errors.add(:release_year, "Can't be blank if released")
+        end
+    end
+end
