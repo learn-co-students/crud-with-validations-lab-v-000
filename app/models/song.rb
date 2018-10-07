c444d24b0146b89fdbcdc7bc4511cdbb519009c1
Joana Palero <joana.palero24@gmail.com>
2018-10-07 11:19:35 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,12 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: { scope: :release_year }
+  validates :release_year, presence: true, if: :released?
+  validates :released, inclusion: { in: [true, false] }
+  validate :release_year_cannot_be_in_future
+
+def release_year_cannot_be_in_future
+  if release_year.present? && release_year > Date.today.year
+    errors.add(:release_year, "can't be in the future")
+    end
+  end
+end
