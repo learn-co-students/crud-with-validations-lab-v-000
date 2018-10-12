317cca784405aa826d0a67f8e72b84bd5816ae6a
github username <github email address>
2018-10-12 14:43:38 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,10 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :artist_name, uniqueness: true
-   
+  validates :title, uniqueness: true
+  validates :artist_name, presence: true
+
+  with_options if: :released do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: { less_than_or_equal_to: Time.now.year }
+  end
 end
