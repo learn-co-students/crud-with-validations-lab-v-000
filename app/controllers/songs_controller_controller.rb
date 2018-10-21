c8d3bcec41ff0a690de5a5b2d9903ac7facb5532
Pamela Frieze <pfrieze@gmail.com>
2018-10-21 21:32:09 +0000

    Done.

---
+++ app/controllers/songs_controller_controller.rb
@@ -0,0 +1,10 @@
+class SongsControllerController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def edit
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,8 @@
 Rails.application.routes.draw do
+  get 'songs_controller/index'
+
+  get 'songs_controller/show'
+
+  get 'songs_controller/edit'
+
 end
