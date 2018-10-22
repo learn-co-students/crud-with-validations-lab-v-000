256b2409353fe0c732d84087dffba6d550651f91
Pamela Frieze <pfrieze@gmail.com>
2018-10-22 20:06:00 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,48 @@
-class SongsController < ActionController::Base
-  validates :title, presence: true, uniqueness: true
-  validates :released, 
-  validates :artist_nam, presence: true
+class SongsController < ApplicationController
+  def index
+    @songs = Song.all
+  end
+
+  def show
+    @song = Song.find(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def create
+       @song = Song.new(song_params)
+    if @song.save
+      redirect_to @song
+    else
+      render :new
+    end
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to songs_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    Song.find(params[:id]).destroy
+    redirect_to songs_url
+  end
+
+  def song_params
+    params.require(:song).permit(
+      :title, :release_year, :released, :genre, :artist_name
+    )
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  get 'songs_controller/index'
+  get '/songs', to: 'songs#index'
+  get '/songs/new', to: 'songs#new', as: 'new_song'
+  post '/songs', to: 'songs#create'
+  get '/songs/:id', to: 'songs#show', as: 'song'
+  get '/songs/:id/edit', to: 'songs#edit', as: 'edit_song'
+  patch '/songs/:id', to: 'songs#update'
+  delete '/songs/:id', to: 'songs#destroy'
 
-  get 'songs_controller/show'
-
-  get 'songs_controller/edit'
-
+  root 'songs#index'
 end
