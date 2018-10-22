179617ef9cf3bc8e51832308dc0d5aed38bf399e
Pamela Frieze <pfrieze@gmail.com>
2018-10-22 20:15:26 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -27,7 +27,7 @@ class SongsController < ApplicationController
   def update
     @song = Song.find(params[:id])
     if @song.update(song_params)
-      redirect_to songs_path
+      redirect_to @song
     else
       render :edit
     end
@@ -37,6 +37,8 @@ class SongsController < ApplicationController
     Song.find(params[:id]).destroy
     redirect_to songs_url
   end
+
+  private 
 
   def song_params
     params.require(:song).permit(
