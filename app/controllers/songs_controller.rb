4be2478341e06782770b4f52019d32b281ff186f
github username <github email address>
2018-11-17 13:18:58 -0800

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,24 @@
 class SongsController < ApplicationController
+  before_action :set_song!, only: [:show, :edit, :update]
+
+  def index
+    @songs = Song.all
+  end
+
+  def show  
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def edit
+  end
+
+  private
+
+  def set_song!
+    @song = Song.find(params[:id])
+  end
+
 end
