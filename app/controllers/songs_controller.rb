814cee231690e9d3f6392360173c51571e5e7f35
Brian Birdwell <bbirdwell@gmail.com>
2018-11-22 18:12:35 +0000

    add shells for controller methods

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,22 @@
 class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def delete
+  end
+  
 end
