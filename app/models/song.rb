fbf22b52f4b75fc3833c86ad510d200e04b27c39
Brian Birdwell <bbirdwell@gmail.com>
2018-11-22 18:47:54 +0000

    complete controller and template specs

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :released, inclusion: { in: %w(true false)}
+  validates :released, inclusion: { in: [true, false]}
   validates :release_year, presence: true, if: :released?
   validates :release_year, numericality: { less_than_or_equal_to: 2018}
   validates :artist_name, presence: true
