dac646156aa7817956b0eb61ba59115734e381b9
jinjo39 <c.delavar@gmail.com>
2018-11-24 13:30:35 -0800

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,13 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true, uniqueness: { scope: :release_year, message: "should happen once per year" }
+  validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }, presence: true, if: :released?
+  validates :artist_name, presence: true
+
+  def released?
+    released == true
+  end
+
+
+end
+
+
