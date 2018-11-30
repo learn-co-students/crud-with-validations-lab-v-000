741a36ecab1ab069a3fdc7e5d153e20bdd713fff
lauradarlak <lauradarlak@gmail.com>
2018-11-30 17:52:09 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,9 @@
 class Song < ActiveRecord::Base
-  validates :title, presence: true, uniqueness: true
+  validates :title, presence: true
+  validates :title, uniqueness: {
+    scope: %i[release_year artist_name],
+    message: 'cannot be repeated by the same artist in the same year'
+  }
   validates :released, inclusion: { in: [true, false] }
   validates :release_year, presence: true, if: :released
   validate :release_year_cannot_be_past
