7b8fc58f6bfb13d15bf898a3bc2c87872d4bcb53
Nadiafa <nadiafa581@gmail.com>
2018-12-17 10:46:24 +0000

    Done.

---
+++ spec/helpers/songs_helper_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+# Specs in this file have access to a helper object that includes
+# the SongsHelper. For example:
+#
+# describe SongsHelper do
+#   describe "string concat" do
+#     it "concats two strings with spaces" do
+#       expect(helper.concat_strings("this","that")).to eq("this that")
+#     end
+#   end
+# end
+RSpec.describe SongsHelper, type: :helper do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
+
+describe Song do
+  let(:attributes) do
+    {
+      title: "Once",
+      released: true,
+      release_year: 1991,
+      artist_name: "Pearl Jam",
+      genre: "Rock"
+    }
+  end
+
+  it  "is considered valid" do 
+    expect(Song.new(attributes)).to be_valid
+  end 
+end 
