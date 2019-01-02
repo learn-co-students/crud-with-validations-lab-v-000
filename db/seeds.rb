4d8a4f41838d293f0864e44ae624c7bb635cf921
JustineGB <justinegayton@gmail.com>
2019-01-02 14:15:51 -0700

    completed lab

---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Song.create(title: "Brokedown Palace", artist_name: "Grateful Dead", release_year: 1970, released: true, genre: "Folk-Rock")
+Song.create(title: "I Will Always Love You", artist_name: "Whitney Houston", release_year: 1992, released: true, genre: "Pop")
+Song.create(title: "Thriller", artist_name: "Michael Jackson", release_year: 1982, released: true, genre: "Rock")
