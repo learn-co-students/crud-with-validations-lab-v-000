2a77e6e28b13af944fe5075e440e50771343f5ba
Justine B <justinegayton@gmail.com>
2019-01-02 21:23:44 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,5 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-<<<<<<< HEAD
   validates :title, uniqueness: { scope: [:release_year, :artist_name]}
   validates :artist_name, presence: true
   validates :released, inclusion: { in: [true, false] }
@@ -8,8 +7,3 @@ class Song < ActiveRecord::Base
   validates :release_year, numericality: { less_than_or_equal_to: 2019}, if: :released
 
 end
-=======
-  validates :title, uniqueness: { scope: :year }
-  validates :artist_name, presence: true
-end 
->>>>>>> 333f89375305086a140d6e5edb3424c106af40dc
