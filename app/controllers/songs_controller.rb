e8ff0ebb7cd89993e5382d59238ad8b5d10a2149
RonSala <ron.sala@gmail.com>
2019-01-06 05:05:05 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,8 +10,19 @@ class SongsController < ApplicationController
   end
 
   def edit
+    set_song
   end
 
   def delete
+  end
+
+  private
+
+  def set_song
+    @song = Song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,4 +3,16 @@ class Song < ActiveRecord::Base
   validates :artist_name, presence: true
   validates :release_year, presence: true, if: :released
   validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }, if: :released
+  # validate :unique_by_artist_in_year?
+
+  # def unique_by_artist_in_year?
+  #   songs = Song.all
+  #   songs.each do | song |
+  #     if song.release_year == :release_year
+  #       valid? = false
+  #     end
+  #   end
+  # end
+
+
 end
