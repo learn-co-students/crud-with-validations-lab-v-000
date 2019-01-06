e0b5235d72474889089d23ef871c9d0be860bee8
RonSala <ron.sala@gmail.com>
2019-01-06 03:24:39 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :released, presence: true
+  # validates :released, presence: true
   validates :artist_name, presence: true
   validates :release_year, presence: true, if: :released
 end
