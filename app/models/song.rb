6d6316c741a70ac52f2ec5124e1aa3d27d6741df
RonSala <ron.sala@gmail.com>
2019-01-06 03:39:34 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,6 +1,6 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  # validates :released, presence: true
   validates :artist_name, presence: true
   validates :release_year, presence: true, if: :released
+  validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }, if: :released
 end
