1c2f08307a125ace492482958eeb7d45cb55e3dc
Melissa Real <melissa.real@hotmail.com>
2019-01-10 15:45:35 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -5,9 +5,7 @@ class Song < ActiveRecord::Base
    }
     validates :released, inclusion: { in: [ true, false ] }
     validates :release_year, presence: true, if: -> { self[:released] }
-    validates :release_year, numericality: {
-     less_than_or_equal_to: Date.today.year
-    }, if: -> { self[:released] }
+    validates :release_year, numericality: { less_than_or_equal_to: Date.today.year}, if: -> { self[:released] }
     validates :artist_name, presence: true
 
 
