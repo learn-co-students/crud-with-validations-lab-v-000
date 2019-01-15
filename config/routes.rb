91d4dd5cdd77b7f7d8c55f9f58937dcedc76db0e
kev112 <werewaswas@live.com>
2019-01-14 21:54:38 -0800

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,13 @@
 Rails.application.routes.draw do
+  get 'songs/index'
+
+  get 'songs/show'
+
+  get 'songs/edit'
+
+  get 'songs/new'
+
+  resources :songs
+  resources :songs
+  resources :songs
 end
