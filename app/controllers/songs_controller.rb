56b9247a3473988eaf3ef3808b55731b0cb6bd93
Barbara Choy <barbchoy@gmail.com>
2019-01-25 22:34:14 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -5,19 +5,25 @@ class SongsController < ApplicationController
   end
 
   def create
-
+    @song = Song.new(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
   end
 
   def new
-
+    @song = Song.new
   end
 
   def edit
-
+    @song = Song.find(params[:id])
   end
 
   def show
-
+    @song = Song.find(params[:id])
   end
 
   def update
@@ -26,5 +32,11 @@ class SongsController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def song_params
+    params.permit(:title, :released, :release_year, :artist_name, :genre)
   end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,6 @@
 class Song < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :artist_name, presence: true
+
 end
