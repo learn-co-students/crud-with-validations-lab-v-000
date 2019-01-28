139018c4bb4efbfe137df4abce204bedd39613e9
Nikki A <nikki.apostolakis@gmail.com>
2019-01-28 23:39:38 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,9 @@
+class Song < ActiveRecord::Base
+  validates :title, presence: true
+  validates :released, inclusion: { in: [true, false]}
+  validates :artist_name, presence: true
+  validates :release_year, presence: true, if: :released?
+  validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }, if: :released?
+  validates :title, uniqueness: { scope: [:release_year, :artist_name]}
+
+end
