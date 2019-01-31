caee740bb9602631e65c8b1ce7f058e2fb53d5fb
Grace S <vocalise@gmail.com>
2019-01-30 23:50:50 -0800

    Added routes to song resource in config routes.rb file and created routes in Song Controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,46 @@
 class SongsController < ApplicationController
+
+    def index
+        @songs = Song.all
+    end
+
+    def new
+        @song = Song.new(song_params)
+    end
+
+    def show
+        @song = Song.find_by(params[:id])
+    end
+
+    def create
+        @song = Song.new(song_params)
+        if @song.valid?
+            @song.save
+            redirect_to song_path(@song)
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @song = Song.find_by(params[:id])
+    end
+
+    def update
+        @song = Song.find_by(params[:id])
+        @song.assign_attributes(song_params)
+        if @song.valid?
+            @song.save
+            redirect_to song_path(@song)
+        else
+            render :edit
+        end
+    end
+
+    def delete
+    end
+
+    def song_params(*args)
+        params.require(:song).permit(*args)
+    end
 end
