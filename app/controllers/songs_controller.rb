a891a08ac6c77941f91f62e7309f8618a67a5e27
Erin Green <erin.y.green@gmail.com>
2019-02-11 20:31:44 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,11 +1,17 @@
 class SongsController < ApplicationController
 
   def new
-
+    @song = Song.new 
   end
 
   def create
+    @song = Song.new(song_params)
 
+    if @song.save 
+      redirect_to song_path(@song)
+    else
+      render :new 
+    end
   end
 
   def edit
