0e76fd946fb55893accac4ff3ed9f586e627d2b7
Erin Green <erin.y.green@gmail.com>
2019-02-11 20:40:03 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,7 @@
 class SongsController < ApplicationController
 
   def new
-    @song = Song.new 
+    @song = Song.new(song_params)
   end
 
   def create
@@ -19,11 +19,23 @@ class SongsController < ApplicationController
   end
 
   def update
+    @song = Song.find(params[:id])
+    @song.update(song_params)
 
+    if @song.save 
+      redirect_to song_path(@song)
+    else 
+      render :edit
+    end
+    
   end
 
   def delete
 
+  end
+
+  def song_params
+    params.permit(:title, :artist_name, :released, :release_year, :genre )
   end
 
 end
