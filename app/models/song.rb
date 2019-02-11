cd4eccf0ef8d6f918ec7460e9c2789cea0d97f0b
Erin Green <erin.y.green@gmail.com>
2019-02-11 20:19:34 +0000

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  validates :title, uniqeness: true
-  validates :released, inclusion: {in: %w(true false)}
+  validates :title, uniqueness: {scope: [:release_year, :artist_name]}
+  validates :released, inclusion: {in: %w(True False)}
   validates :artist_name, presence: true
   validate :optional_release_year
 
