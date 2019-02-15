3ed987ab2c419653adf3667db4c44c65fd138a14
github username <github email address>
2019-02-15 09:31:59 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,24 +4,10 @@ class Song < ActiveRecord::Base
   validates :released, inclusion: { in: [ true, false ] }
   validates :release_year, presence: true, numericality: { less_than_or_equal_to: Time.current.year}, if: :released
   validates :artist_name, presence: true
-  validates :title, uniqueness: true if : :same_year?
+  validates :title, uniqueness: true, if: :same_year?
 
   def same_year?
-    binding.pry
-
-
-  #   if Song.all.any? {|song| self.title.include?(song)}
-  #     release_year.uniq
-  #   else
-  #     errormessage
-  #   end
+    Song.all.any? {|song|song.release_year.to_s.include? release_year.to_s}
   end
 
 end
-
-
-# if self.title
-#       unless clickbaity_words.any? {|word| self.title.include?(word)}
-#         errors.add(:title, "must be clickbaity")
-#       end
-#     end
