ab1ef97fcf25c5636072b18ada25daa8d1aecb20
github username <github email address>
2019-02-15 13:21:11 -0600

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -2,11 +2,20 @@ class Song < ActiveRecord::Base
   validates :title, presence: true, uniqueness: true
   # validates :title, uniqueness: true
   validates :released, inclusion: { in: [true, false]}
+  # Optional if released is false
   validates :release_year, presence: true, if: :been_released?
 
 
+# for release_year
+  # Optional if released is false
+
+  # Must not be blank if released is true
+
+  # Must be less than or equal to the current year
+
 
   def been_released?
+    binding.pry
     self.released
   end
 
