96b7d0f80dee01f2e5fc93b1ec20948c5317d8bc
RonSala <ron.sala@gmail.com>
2019-02-19 01:28:11 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,17 +3,17 @@ class Song < ActiveRecord::Base
   validates :artist_name, presence: true
   validates :release_year, presence: true, if: :released
   validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }, if: :released
-  validates :release_year, uniqueness: true, if :
+  # validates :release_year, uniqueness: true, if :
 
 
-  # validate :unique_by_artist_in_year?
+  validate :unique_by_artist_in_year?
 
-  # def unique_by_artist_in_year?
-  #   songs = Song.all
-  #   songs.each do | song |
-  #     if song.release_year == :release_year && song.title == :title
-  #       valid? = false
-  #     end
-  #   end
-  # end
+  def unique_by_artist_in_year?
+    songs = Song.all
+    # songs.each do | song |
+    #   if song.release_year == :release_year && song.title == :title
+    #     valid? = false
+    #   end
+    # end
+  end
 end
