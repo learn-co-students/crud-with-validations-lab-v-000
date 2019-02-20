7b158379108cca0a2aeb503e5a2b65c6b56ecd7e
RonSala <ron.sala@gmail.com>
2019-02-19 22:26:11 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,19 +1,29 @@
 class Song < ActiveRecord::Base
+
+  # extend ActiveModel::Naming
+
+  # def initialize
+  #   @errors = ActiveModel::Errors.new(self)
+  # end
+
+  # attr_accessor :name
+  # attr_reader   :errors
+
   validates :title, presence: true
   validates :artist_name, presence: true
   validates :release_year, presence: true, if: :released
   validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }, if: :released
-  # validates :release_year, uniqueness: true, if :
-
 
   validate :unique_by_artist_in_year?
 
   def unique_by_artist_in_year?
     songs = Song.all
-    # songs.each do | song |
-    #   if song.release_year == :release_year && song.title == :title
-    #     valid? = false
-    #   end
-    # end
+    songs.each do | s |
+      if s.release_year == release_year && s.title == title && s.artist_name == artist_name
+        binding.pry
+        # errors.add(:release_year, “cannot put out the same song the same year”)
+        # binding.pry
+      end
+    end
   end
 end
