2a5414a4e71522abd4c7724e41bfc716cd0fe99a
RonSala <ron.sala@gmail.com>
2019-02-21 21:57:24 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,14 +1,5 @@
 class Song < ActiveRecord::Base
 
-  # extend ActiveModel::Naming
-
-  # def initialize
-  #   @errors = ActiveModel::Errors.new(self)
-  # end
-
-  # attr_accessor :name
-  # attr_reader   :errors
-
   validates :title, presence: true
   validates :artist_name, presence: true
   validates :release_year, presence: true, if: :released
@@ -20,8 +11,7 @@ class Song < ActiveRecord::Base
     songs = Song.all
     songs.each do | s |
       if s.release_year == release_year && s.title == title && s.artist_name == artist_name
-        # errors.add(:release_year, ‘cannot put out the same song the same year’)
-        errors.add(base: "cannot put out the same song the same year")
+        errors.add(:base)
       end
     end
   end
