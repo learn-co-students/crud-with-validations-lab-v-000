b2be48210095b87c56442f993389c3d9c892fd88
ricecake88 <vocalise@gmail.com>
2019-02-22 17:45:27 -0800

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,7 @@
 class SongsController < ApplicationController
 
+    include SongsHelper
+
     def index
         @songs = Song.all
     end
@@ -14,8 +16,8 @@ class SongsController < ApplicationController
 
 
     def create
-        @song = Song.new(song_params(:title, :artist_name, :genre, :released))
-        if @song.valid? && not_released_this_year(params)
+        @song = Song.new(song_params(:title, :artist_name, :released, :release_year, :genre, :released))
+        if @song.valid?
             @song.save
             redirect_to song_path(@song)
         else
@@ -29,9 +31,9 @@ class SongsController < ApplicationController
 
     def update
         @song = Song.find_by(:id => params[:id])
-        @song.assign_attributes(song_params(:title, :artist_name, :genre, :released, \
+        @song.assign_attributes(song_params(:title, :artist_name, :released, :release_year, :genre, :released, \
             :release_year, :artist_name))
-        if @song.valid? && not_released_this_year
+        if @song.valid?
             @song.save
             redirect_to song_path(@song)
         else
@@ -48,13 +50,4 @@ class SongsController < ApplicationController
         params.require(:song).permit(*args)
     end
 
-    helpers do
-    private
-        def not_released_this_year(params)
-            @song = Song.find_by(:artist_name => params[:artist_name], \
-                :release_year => params[:release_year], \
-                :title => params[:title])
-            @song.nil?
-        end
-    end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,23 @@
 class Song < ActiveRecord::Base
     validates :title, presence: true
     validates :artist_name, presence: true
-    validates :released, presence: true
+    validates :released, inclusion: {in: [true, false]}
+    validate :release_year_if_in_future_or_unreleased
+    validate :song_already_entered
+
+    def release_year_if_in_future_or_unreleased
+        if released && release_year.nil?
+            errors.add(:release_year, "Cannot have an empty release year if released")      
+        elsif !release_year.nil? && release_year > Date.today.year
+            errors.add(:release_year, "Cannot have a release year in the future")
+        end
+    end
+
+    def song_already_entered
+        @song = Song.find_by(:title => title, :artist_name => artist_name, :release_year => release_year)
+        if !@song.nil?
+            errors[:base] << "Artist cannot have same title released in the same year"
+        end
+    end
+
 end
