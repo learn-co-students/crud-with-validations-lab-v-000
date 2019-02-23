418f18bb6242dddaf98dc0cfb3e23263f1afebaa
ricecake88 <vocalise@gmail.com>
2019-02-22 16:45:11 -0800

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -15,7 +15,7 @@ class SongsController < ApplicationController
 
     def create
         @song = Song.new(song_params(:title, :artist_name, :genre, :released))
-        if @song.valid?
+        if @song.valid? && not_released_this_year(params)
             @song.save
             redirect_to song_path(@song)
         else
@@ -31,7 +31,7 @@ class SongsController < ApplicationController
         @song = Song.find_by(:id => params[:id])
         @song.assign_attributes(song_params(:title, :artist_name, :genre, :released, \
             :release_year, :artist_name))
-        if @song.valid?
+        if @song.valid? && not_released_this_year
             @song.save
             redirect_to song_path(@song)
         else
@@ -46,5 +46,15 @@ class SongsController < ApplicationController
 
     def song_params(*args)
         params.require(:song).permit(*args)
+    end
+
+    helpers do
+    private
+        def not_released_this_year(params)
+            @song = Song.find_by(:artist_name => params[:artist_name], \
+                :release_year => params[:release_year], \
+                :title => params[:title])
+            @song.nil?
+        end
     end
 end
