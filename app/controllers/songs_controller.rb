370a9a623a6a51a5630d1870567639187032add7
catherinism <catcreates13@gmail.com>
2019-03-03 22:07:12 -0500

    Done.

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,32 @@
+class SongsController < ApplicationController
+
+  def index
+    @songs = Song.all
+  end
+
+  def show
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def create
+      @song = Song.new(song_params)
+      @song.save
+      redirect_to song_path(@song)
+    end
+
+  def update
+      @song = Song.find(params[:id])
+      @song.update(song_params)
+      redirect_to song_path(@song)
+  end
+
+
+
+end
