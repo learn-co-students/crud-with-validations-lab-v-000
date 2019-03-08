eccd98b9e63efd2ebe30d7ba2a0cddbb82fa7512
cvaleriac <cvaleriac@hotmail.it>
2019-03-08 07:15:17 -0800

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,8 @@
+class Song < ActiveRecord::Base
+    validates :title, presence: true
+    validates :title, uniqueness: { scope: :release_year} 
+    validates :released, inclusion: {in: [true, false]}
+    validates :artist_name, presence: true
+    validates :release_year, presence: true, numericality: { only_integer: true, less_than_or_equal_to: Time.now.year}, if: ->{ released }
+
+end
