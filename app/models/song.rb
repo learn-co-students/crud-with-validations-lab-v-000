2986b49e2ec7548e6fab4a9a91ba48750b1eda7d
lpzraf <lpz.raf@gmail.com>
2019-03-09 21:04:32 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,12 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
   validates :artist_name, presence: true
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+  end
+  validates :title, uniqueness: true
+
+  def released?
+    released
+  end
 end
