720055c7b580b28331ab2c4f24e7f57bec4700bb
lpzraf <lpz.raf@gmail.com>
2019-03-09 20:46:15 -0500

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -22,6 +22,21 @@ class SongsController < ApplicationController
     end
   end
 
+  def edit
+    @song = Song.find(params[:id])
+  end
+
+  def update
+    @song = Song.find(params[:id])
+    @song.update(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
   private
   def song_params
     params.permit(:title, :genre, :artist_name, :released, :release_year)
