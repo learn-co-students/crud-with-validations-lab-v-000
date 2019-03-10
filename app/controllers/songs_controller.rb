51a341495c51d914efab9479b77ab81152a4d6fa
lpzraf <lpz.raf@gmail.com>
2019-03-10 11:24:19 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -14,8 +14,7 @@ class SongsController < ApplicationController
   def create
     @song = Song.new(song_params)
 
-    if @song.valid?
-      @song.save
+    if @song.save
       redirect_to song_path(@song)
     else
       render :new
@@ -29,13 +28,18 @@ class SongsController < ApplicationController
   def update
     @song = Song.find(params[:id])
     @song.update(song_params)
-    if @song.valid?
-      @song.save
+    if @song.save
       redirect_to song_path(@song)
     else
       render :edit
     end
   end
+
+  def destroy
+    @song = Song.find(params[:id]).destroy
+   redirect_to songs_url
+  end
+
 
   private
   def song_params
