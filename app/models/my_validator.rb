eb1f3cb535deb62908e88c1969e5af8631341979
Leslie Stevens <flymaven@protonmail.com>
2019-03-14 19:35:48 -0500

    edit validations and add routes

--- app/models/my_validator.rb
+++ app/models/my_validator.rb
@@ -1,9 +1,7 @@
 class MyValidator < ActiveModel::Validator
   def validate(song)
-    unless song.author_name.unique? && song.release_year.unique?
-      unless post.title.include? "Won't Believe"
-        post.errors[:title] << 'false'
-      end
+    unless song.author_name.unique? | song.release_year.unique?
+        song.errors[:title] << 'false'
+    end
    end
  end
-end
--- app/models/song.rb
+++ app/models/song.rb
@@ -3,8 +3,8 @@ class Song < ActiveRecord::Base
   validates_with MyValidator
 
   validates :title, presence: true
-  validates :title, uniqueness: true if 
+  validates :release_year, numericality: {less_than_or_equal_to: Date.today.year}
   validates :artist_name, presence: true
   validates :released, inclusion: { in: %w(true false)}
-  validates :release_year presence: true, if: :released?
+  validates :release_year, presence: true, if: :released?
 end
