1593e9db02f66c85281888a3f419d77ca37b783c
AlwinaO <aoyewole2586@gmail.com>
2019-03-23 17:07:06 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -9,11 +9,17 @@ class SongsController < ApplicationController
   end
 
   def new
-
+    @song = Song.new
   end
 
   def create
-
+    @song = Song.new(song_params)
+    if @song.valid?
+      @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end 
   end
 
   def edit
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,12 @@
 class Song < ActiveRecord::Base
+
+  validates :title, presence: true
+  validates :title, uniqueness: { scope: :artist_name, :release_year}
+
+  validates :released, inclusion { in: [true, false] }
+
+  validates :release_year, numericality: true, {if: :released, only_integer: true, less_than_or_equal_to: Time.now.year}
+
+
+
 end
