4a8d27b02b0b3b1294d08ee9be42f78b9bd72e12
danetter17 <etter.dt@gmail.com>
2019-03-26 11:58:40 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -6,6 +6,12 @@ class Song < ActiveRecord::Base
   }
   validates :released, inclusion: { in: [true, false] }
   validates :artist_name, presence: true
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: {
+      less_than_or_equal_to: Date.today.year
+    }
+  end
 
   def released?
     released
