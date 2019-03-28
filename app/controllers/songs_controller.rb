fe93b0282e06159f2e27e287718f6520eb6e9ca6
Guess <guess@Guesss-iMac.local>
2019-03-27 22:20:49 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,4 @@
 class SongsController < ApplicationController
-
 	def index
 		@songs = Song.all
 	end
@@ -12,42 +11,40 @@ class SongsController < ApplicationController
 		@song = Song.new
 	end
 
+	def edit
+		@song = Song.find(params[:id])
+	end
+
 	def create
 		@song = Song.new(song_params)
-		if @song.valid?
 			@song.save
-			redirect_to song_path(@song)
+			redirect_to @song
 		else
 			render :new
 		end
 	end
 
-	def edit
-		@song = Song.find(params[:id])
-	end
-
 	def update
-		@song = Song.find(params[:id])
-		if @song.update(song_params)
-      redirect_to song_path(@song)
+    @song = Song.find(params[:id])
+    if @song.update(song_params)
+      redirect_to @song
     else
       render :edit
     end
   end
 
-
 	def destroy
     @song = Song.find(params[:id])
     @song.destroy
-    redirect_to songs_path
+    redirect_to songs_url
   end
 
+  private
 
-	  private
+  def song_params
+    params.require(:song).permit(
+      :title, :release_year, :released, :genre, :artist_name
+    )
+  end
 
-	  def song_params
-	    params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
-	  end
-
-
-	end
+end
--- db/migrate/20190328014036_create_songs.rb
+++ db/migrate/20190328014036_create_songs.rb
@@ -1,13 +1,13 @@
 class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
-       t.string :title
-       t.boolean :released
-       t.integer :release_year
-       t.string :artist_name
-       t.string :genre
+      t.string :title
+      t.integer :release_year
+      t.boolean :released
+      t.string :genre
+      t.string :artist_name
 
-       t.timestamps null: false
-     end
-   end
- end
+      t.timestamps null: false
+    end
+  end
+end
