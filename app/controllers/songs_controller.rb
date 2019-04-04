abc9209f016de323844fdcac7176959ea5a6fb49
Jonathan Luk <jluk87@gmail.com>
2019-04-04 01:02:25 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,2 +1,45 @@
 class SongsController < ApplicationController
+
+  before_action :set_song, only: [:show, :edit, :update, :delete]
+
+  def index
+    @songs = Song.all
+  end
+
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song = Song.new(song_params)
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def delete
+    @song.destroy
+  end
+
+  private
+
+  def set_song
+    @song = Song.find_by_id(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit!
+  end
+
+
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,28 @@
 class Song < ActiveRecord::Base
+  validates :title,:artist_name, presence: true
+  validate :check_title_for_string_type, :check_artist_name_for_string_type
+  validate :unique_title_for_year
+  validates :released, inclusion: { in: [true, false], message: "value is not true or false"}
+  validates :release_year, presence: true, if: :song_released?
+  validates :release_year, numericality: { less_than_or_equal_to: DateTime.now.strftime("%Y").to_i},
+    unless: Proc.new{|a| a.release_year.blank?}
+
+  def unique_title_for_year
+    if Song.all.any? {|song| song.title == title && song.release_year == release_year}
+      errors.add(:title, "song title with the same artist and release year already exists")
+    end
+  end
+
+  def song_released?
+    released == true
+  end
+
+  def check_title_for_string_type
+    errors.add(:title, 'title should be a string') unless title.is_a?(String)
+  end
+
+  def check_artist_name_for_string_type
+    errors.add(artist_name, 'artist_name should be a string') unless artist_name.is_a?(String)
+  end
+
 end
