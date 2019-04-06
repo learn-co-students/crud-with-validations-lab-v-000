e96a036f0ba2ec12ba4fd5f5ee64519938c8876e
Ashley <ashleywong678@gmail.com>
2019-04-06 15:17:41 -0400

    Done.

--- db/migrate/20190406161940_create_songs.rb
+++ db/migrate/20190406161940_create_songs.rb
@@ -1,4 +1,4 @@
-class CreateSongs < ActiveRecord::Migration
+class CreateSongs < ActiveRecord::Migration[5.0]
   def change
     create_table :songs do |t|
       t.string :title
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -11,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190406161940) do
+ActiveRecord::Schema.define(version: 2019_04_06_161940) do
 
   create_table "songs", force: :cascade do |t|
-    t.string   "title"
-    t.boolean  "released"
-    t.integer  "release_year"
-    t.string   "artist_name"
-    t.string   "genre"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.string "title"
+    t.boolean "released"
+    t.integer "release_year"
+    t.string "artist_name"
+    t.string "genre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
