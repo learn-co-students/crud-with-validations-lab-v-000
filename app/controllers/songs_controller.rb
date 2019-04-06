2365187f94c20cbcd34b42944c95f50a83caa5fa
Ashley <ashleywong678@gmail.com>
2019-04-06 16:23:10 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -4,14 +4,21 @@ class SongsController < ApplicationController
     @songs = Song.all
   end
 
+  def new
+  end
+  
+  def create
+  end
+  
   def show
     @song = Song.find(params[:id])
   end
 
-  def new
-  end
 
-  def create
-  end
+
+  private
+    def song_params
+      params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+    end
 
 end
