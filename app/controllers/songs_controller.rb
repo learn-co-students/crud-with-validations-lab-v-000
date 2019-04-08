8a8d599b4092bcde7f6cfc7c597c4a4a8c27bce9
anihakutin <4hbrody@gmail.com>
2019-04-07 22:46:42 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,29 +1,51 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show, :edit, :update, :destroy]
+
   def index
-
-  end
-
-  def create
-
+    @songs = Song.all
   end
 
   def new
-
+    @song = Song.new
   end
 
-  def edit
+  def create
+    @song = Song.new(song_params)
 
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      render :new
+    end
   end
 
   def show
+  end
 
+  def edit
   end
 
   def update
+    if @song.update(song_params)
 
+      redirect_to song_path(@song)
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @song.destroy
 
+    redirect_to songs_path
   end
+
+  private
+    def set_song
+      @song = Song.find(params[:id])
+    end
+
+    def song_params
+      params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
+    end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,24 +1,12 @@
 class Song < ActiveRecord::Base
-  validates :title, format: { with: /[A-Za-z]/}, uniqueness: { scope: :release_year }
-  validates :release_year, presence: true, if: :released?
-  validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }
+  validates :title, format: { with: /[A-Za-z]/}, uniqueness: { scope: %w[release_year artist_name] }
+
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true
+    song.validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }
+  end
 
   def released?
     released
   end
-
-  # - `title`, a `string`
-  #   - Must not be blank
-  #   - Cannot be repeated by the same artist in the same year
-  # - `released`, a `boolean` describing whether the song was ever officially
-  #   released.
-  #   - Must be `true` or `false`
-  # - `release_year`, an `integer`
-  #   - Optional if `released` is `false`
-  #   - Must not be blank if `released` is `true`
-  #   - Must be less than or equal to the current year
-  # - `artist_name`, a `string`
-  #   - Must not be blank
-  # - `genre`, a `string`
-
 end
