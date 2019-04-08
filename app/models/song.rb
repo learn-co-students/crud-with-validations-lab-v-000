547d5d8eeff278ca4cbca0ff9365921f6f1c0b4b
Ashley <ashleywong678@gmail.com>
2019-04-08 15:31:33 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,30 +1,22 @@
 class Song < ActiveRecord::Base
   validates :title, presence: true
-  # validates :title (cannot be repeated by same arist in the same year)
-  # validates :unique_title, confirmation: true
   validates :released, inclusion: [true, false]
   validates :release_year, presence: true, if: :released
-    validates :past_year, confirmation: true
+  validates :past_year, confirmation: true
   validates :artist_name, presence: true
+  validates :unique_title, confirmation: true
 
   def past_year
-    if release_year
-      if release_year != Time.now.year && release_year > Time.now.year
-        errors[:release_year] << "Year can't be in the future"
-      end
+    if release_year && release_year != Time.now.year && release_year > Time.now.year
+      errors[:release_year] << "Year can't be in the future"
     end
+  end
 
   def unique_title
-    binding.pry
-    song = Song.find_by(title)
-    if title && song
-      if song.release_year == release_year && song.artist_name == artist_name
-        errors[:title] << "Cannot have the same song title in the same year by the same artist"
-      end
+    song = Song.find_by(title: title)
+    if title && song && release_year == song.release_year && artist_name == song.artist_name
+        errors[:title] << "That song was released this year"
     end
   end
-
-  end
-
 
 end
