b0454b64189c41487606c4c88dc77713db33bf6d
Meg Gutshall <meghan.gutshall@gmail.com>
2019-04-20 23:12:33 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,14 @@
 class Song < ApplicationRecord
+  validates :title, presence: true
+  validates :released, inclusion: {in: [true, false]}
+  validates :release_year, presence: true, length: {maximum: 2019}, if: :released?
+  validates :artist_name, presence: true
+
+  # Custom Validations
+
+  def repeat?
+    
+  end
+
+
 end
