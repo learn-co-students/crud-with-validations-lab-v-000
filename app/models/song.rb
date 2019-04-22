7dd3fd0628665537cc3b766bb0bff8d39def6756
Meg Gutshall <meghan.gutshall@gmail.com>
2019-04-21 23:04:19 -0400

    Create song model validations

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,14 +1,16 @@
 class Song < ApplicationRecord
   validates :title, presence: true
+  validates :title, uniqueness: {scope: [:artist_name, :release_year], message: "cannot be released under the same artist in the same year"}
   validates :released, inclusion: {in: [true, false]}
-  validates :release_year, presence: true, length: {maximum: 2019}, if: :released?
+  with_options if: :released? do |song|
+    song.validates :release_year, presence: true, numericality: {less_than_or_equal_to: Date.today.year }
+  end
   validates :artist_name, presence: true
 
-  # Custom Validations
+  # Validation Helper
 
-  def repeat?
-    
+  def released?
+    released
   end
-
 
 end
