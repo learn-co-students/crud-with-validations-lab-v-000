916ad959071c2b9e05d294e246ce55d106fd8062
Amanda Murphy <helloamandamurphy@gmail.com>
2019-05-03 17:49:29 -0400

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190503213818) do
+
+  create_table "songs", force: :cascade do |t|
+    t.string  "title"
+    t.integer "release_year"
+    t.boolean "released"
+    t.string  "genre"
+    t.string  "artist_name"
+  end
 
 end
