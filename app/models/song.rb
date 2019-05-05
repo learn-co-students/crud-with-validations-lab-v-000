6fb5e22f8538f3eeaaf84c74a66e63b6a99d75aa
github username <github email address>
2019-05-05 14:36:44 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,5 +1,15 @@
 class Song < ApplicationRecord
   validates :title, presence: true
-#  validates :title, uniqueness: true { scope: :release_year, :artist_name }
+#  validates :title, uniqueness: true
+  validate :invalid_without_release_year
+  validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }
+  #is valid without release year when released is false (FAILED - 3)
+
+    def invalid_without_release_year
+      if released == true && release_year = nil
+        errors.add(:release_year, "must have a release year")
+      end
+    end
+
 
 end
