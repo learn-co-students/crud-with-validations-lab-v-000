67254a6fc227738d3273bedda9906cdb94b741ef
Patrick McCombs <pa.mccombs1@gmail.com>
2019-05-31 07:35:17 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190531064235) do
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "title"
+    t.boolean  "released"
+    t.integer  "release_year"
+    t.string   "artist_name"
+    t.string   "genre"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end
