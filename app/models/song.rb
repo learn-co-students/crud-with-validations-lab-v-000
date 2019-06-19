2e54e320b01dbb7c7bba30f347347cf426e7abda
MisterSenor <nathan.visak@gmail.com>
2019-06-19 09:53:20 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,17 @@
 class Song < ApplicationRecord
-  validates :title, presence: true, uniqueness: { scope: :year,
-    message: "A song title cannot be repeated by the same artist in the same year." }
+  validates :title, presence: true,
   validates :released, presence: true, inclusion: { in: [true, false] }
   validates :release_year, presence: true, if: :released == true, numericality: { less_than_or_equal_to: Time.now.year}
   validates :artist_name, presence: true
+  validate :title_repeated_in_same_year
+
+  def title_repeated_in_same_year
+    @songs = Song.all
+    if @songs.any? do |song|
+      song.artist_name == song_params[:artist_name] || song.release_year == song_params[:release_year] || song.title == song_params[:title]
+      end
+      errors.add(:title, "An artist cannot repeat a title in the same year")
+    end
+  end
+
 end
