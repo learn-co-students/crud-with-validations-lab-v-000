48c350c43e44255ca4db1d122e67e41b9f243742
christianavdm <christianavdm@gmail.com>
2019-06-27 23:58:17 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,4 +1,18 @@
 class Song < ApplicationRecord
    validates :title, presence: true
    validates :artist_name, presence: true
+   validate :released?
+   validate :future_release?
+
+   def released?
+    if released == true && release_year == nil
+      errors.add(:release_year, "must have release year")
+    end
+  end
+
+  def future_release?
+    if release_year > Date.today.year
+      errors.add(:release_year, "release year cannot be in the future")
+    end
+  end
 end
