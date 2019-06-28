c48af0e64debbd6c444e40d02c47078e2ea13065
christianavdm <christianavdm@gmail.com>
2019-06-28 00:08:12 -0700

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,6 +3,7 @@ class Song < ApplicationRecord
    validates :artist_name, presence: true
    validate :released?
    validate :future_release?
+   validate :same_song?
 
    def released?
     if released == true && release_year == nil
@@ -13,6 +14,21 @@ class Song < ApplicationRecord
   def future_release?
     if release_year && release_year > Date.today.year
       errors.add(:release_year, "release year cannot be in the future")
+    end
+  end
+  
+  def same_song? 
+    songs = Song.all
+    matching_song = nil
+
+    songs.each do |song|
+        if song.title == title && song.artist_name == artist_name && song.release_year == release_year
+            matching_song = song 
+        end
+    end
+
+    if matching_song
+        errors.add(:release_year, "artist cannot release the same title more than once a year")
     end
   end
 end
