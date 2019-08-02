a7efdbbe9007649e32f39215a589b580a830f2c4
Paul Fell <swellpf@gmail.com>
2019-08-02 14:25:03 -0400

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,7 +6,7 @@ class SongsController < ApplicationController
 
     def create
         @song = Song.new(song_params)
-        if @post.save
+        if @song.save
             redirect_to song_path(@song)
         else
             render :new
