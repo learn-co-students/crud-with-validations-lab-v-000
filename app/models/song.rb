9e2f53bab2838edb5ce9e45b29efccc46231873d
Rachel Grier <rgrier104@gmail.com>
2019-09-20 19:34:53 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,9 @@
+class Song < ApplicationRecord
+  validates :title, presence: true
+  validates :title, uniqueness: { scope: [:release_year, :artist_name] }
+  validates :artist_name, presence: true
+  validates :released, inclusion: { in: [true, false] }
+  validates :release_year, presence: true, if: :released?
+  validates :release_year, numericality: { less_than_or_equal_to: Date.today.year }, if: :released?
+
+end
