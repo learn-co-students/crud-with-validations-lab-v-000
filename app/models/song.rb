f3b295b569bc9f567da052a7c6290e86b1fb3f95
Amil <github email address>
2019-09-28 06:32:45 -0400

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,27 @@
+
+class Song < ApplicationRecord
+  validates :title, :artist_name, presence: true
+  validates :released, exclusion: { in: [nil] }
+  validate :relased
+  validate :release_date
+
+  validates :title, uniqueness: { scope: :release_year,
+   message: "should happen once per year" }
+
+  def relased
+    if released == true && release_year == nil
+      errors.add(:released, "Must be false if there is no relase year")
+    end
+  end
+
+  def release_date
+    if release_year.present? && release_year > Date.today.year
+      errors.add(:release_year, "Can't be in the future")
+    end
+  end
+
+
+
+
+
+end
