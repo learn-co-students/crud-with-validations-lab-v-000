1b611875167f49d9d319ce340172999fcdf3e6af
sdcro <sdcrouse91@gmail.com>
2019-12-06 01:20:08 -0800

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -4,8 +4,10 @@ class Song < ApplicationRecord
   validates :released,
     inclusion: {
       in: [true, false], 
-      # Is there a better way to write this? ANY string value evaluates to true!
+      # Is there a better way to write this? ANY string value besides "false" evaluates to true!
       # If I try 'in: ["true", "false"]', then values of "true" and "false" won't work!
+      # Is this because :released is a "boolean" datatype, rather than "string"? It seems to work with :artist_name.
+      # I found this: https://api.rubyonrails.org/classes/ActiveModel/Type/Boolean.html
       message: "must be 'true' or 'false'"
     }
 end
