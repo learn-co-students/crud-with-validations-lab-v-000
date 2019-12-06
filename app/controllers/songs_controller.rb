18a150170f55794218a9bbba74b5ef8ca866efb2
sdcro <sdcrouse91@gmail.com>
2019-12-05 23:56:53 -0800

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -2,4 +2,12 @@ class SongsController < ApplicationController
   def index
     @songs = Song.all
   end
+
+  def show
+    @song = Song.find_by_id(params[:id])
+  end
+
+  def new
+    @song = Song.new
+  end
 end
