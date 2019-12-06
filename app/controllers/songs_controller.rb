13c803fee09d2a471faa7eacc33944324516e721
sdcro <sdcrouse91@gmail.com>
2019-12-06 01:02:41 -0800

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -10,4 +10,19 @@ class SongsController < ApplicationController
   def new
     @song = Song.new
   end
+
+  def create
+    @song = Song.new(song_params)
+  
+    if @song.save
+      redirect_to song_path(@song)
+    else
+      #raise @song.errors.full_messages.inspect
+      render :new
+    end
+  end
+
+  def song_params
+    params.require(:song).permit(:title, :artist_name, :genre, :released, :release_year)
+  end
 end
--- app/models/song.rb
+++ app/models/song.rb
@@ -1,2 +1,11 @@
 class Song < ApplicationRecord
+  validates :title, presence: true
+  validates :artist_name, presence: true
+  validates :released,
+    inclusion: {
+      in: [true, false], 
+      # Is there a better way to write this? ANY string value evaluates to true!
+      # If I try 'in: ["true", "false"]', then values of "true" and "false" won't work!
+      message: "must be 'true' or 'false'"
+    }
 end
