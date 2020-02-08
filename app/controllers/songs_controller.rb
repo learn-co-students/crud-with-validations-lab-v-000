ab46a838a67e8dd9b393fe714bb5e03a985ff032
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-02-07 20:59:24 -0500

    Create song controller actions.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,6 +1,31 @@
 class SongsController < ApplicationController
+  before_action :find_song, only: [:edit, :show]
 
   def index
     @songs = Song.all
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def create
+    @song = Song.create(post_params)
+    redirect_to song_path(@song)
+  end
+
+
+  private
+
+  def find_song
+    @song = Song.find(params[:id])
+  end
+
+  def post_params
+   params.require(:song).permit(:title, :released, :release_year, :artist_name, :genre)
   end
 end
