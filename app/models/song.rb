af07e0ec1820ee8d16a9a5d2d0f3390830f113a4
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-02-08 14:46:54 -0500

    Add validations to Song model.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,12 @@
 class Song < ApplicationRecord
-  validates :title, :artist_name, :release_year, presence: true
+  validates :title, :artist_name, presence: true
+  validates :title, uniqueness: { scope: [:release_year, :artist_name] }
+
+  validates :release_year, presence: true, if: :released_valid?
+  validates :release_year, numericality: { less_than_or_equal_to: Time.now.year }, allow_nil: true
+
+
+  def released_valid?
+    released
+  end
 end
