0f5088720206cf9709a21fdd97ab598ed1dfe99f
Mariel Hoepelman <marielhoepelman@gmail.com>
2020-02-08 15:45:11 -0500

    Deletes repeated migrations

--- db/migrate/20200208011925_remove_released_year_from_songs.rb
+++
@@ -1,5 +0,0 @@
-class RemoveReleasedYearFromSongs < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :songs, :released_year, :integer
-  end
-end
--- db/migrate/20200208012116_add_release_year_to_songs.rb
+++
@@ -1,5 +0,0 @@
-class AddReleaseYearToSongs < ActiveRecord::Migration[5.0]
-  def change
-    add_column :songs, :release_year, :integer
-  end
-end
