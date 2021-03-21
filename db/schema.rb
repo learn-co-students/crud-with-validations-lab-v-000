f3ddd37b20abd57daacaa9c34f778fc765d20ba6
Laura Messa Travin <laura.travin@gmail.com>
2021-03-21 20:12:33 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 1) do
+
+  create_table "songs", force: :cascade do |t|
+    t.string   "title"
+    t.integer  "release_year"
+    t.boolean  "released"
+    t.string   "genre"
+    t.string   "artist_name"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
 
 end
