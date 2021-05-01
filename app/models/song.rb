d3d6b8968db4d2a79e6635128786fe1a71fe0bf6
Jesse Wenger <wengerj@gmail.com>
2021-05-01 18:29:44 +0000

    Done.

---
+++ app/models/song.rb
@@ -0,0 +1,16 @@
+class Song < ApplicationRecord
+  validates :title, presence: true
+  validates :title, uniqueness: true
+  validates :release_year, presence: true, if: :released?
+  validate :release_year_must_be_in_past
+
+  def released?
+    self.released == true
+  end
+
+  def release_year_must_be_in_past
+    if self.release_year.present? && self.release_year > Date.today.year.to_i
+      errors.add(:release_year, "release_year must be in the past")
+    end
+  end
+end
---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+@song = Song.new
+@song.title = "Test_Title"
+@song.released = true
+@song.release_year = 2000
+@song.artist_name = "Test_Artist"
+@song.genre = "Test_Genre"
+@song.save
